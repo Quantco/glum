9b760dcb8a5cfc302562088e8bb016e48631fad3
Jan Tilly <jantilly@gmail.com>
2020-03-25 20:40:00 +0100

    Remove cookiecutter things we don't need.

--- tests/test_core.py
+++
@@ -1,5 +0,0 @@
-def test_hard():
-    import glm_benchmarks  # noqa
-
-    # this is just a demo that pytest can produce good error messages just by parsing assert statements
-    assert dict(a=1, b=2) == dict(a=1, b=2)
