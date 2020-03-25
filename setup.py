965388b20f112bde12db01955e4e95ef3cf4301e
Jan Tilly <jantilly@gmail.com>
2020-03-25 19:43:24 +0100

    Initial project skeleton

---
+++ setup.py
@@ -0,0 +1,28 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md")) as f:
+    long_description = f.read()
+
+setup(
+    name="glm_benchmarks",
+    use_scm_version={"version_scheme": "post-release"},
+    setup_requires=["setuptools_scm"],
+    description="Python package to benchmark GLM implementations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Quantco/glm_benchmarks",
+    author="QuantCo, Inc.",
+    author_email="noreply@quantco.com",
+    classifiers=[  # Optional
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[],
+)
---
+++ tests/test_core.py
@@ -0,0 +1,5 @@
+def test_hard():
+    import glm_benchmarks  # noqa
+
+    # this is just a demo that pytest can produce good error messages just by parsing assert statements
+    assert dict(a=1, b=2) == dict(a=1, b=2)
