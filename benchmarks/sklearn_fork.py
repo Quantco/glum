107b0b4f3b94ebb9da8af7953155fd8e6476f75a
Ben Thompson <t.ben.thompson@gmail.com>
2020-03-28 10:49:34 -0400

    Simple runtime benchmark. And computing coefficient path for sklearn.

--- benchmarks/sklearn_fork.py
+++ benchmarks/sklearn_fork.py
@@ -1,18 +1,56 @@
+import time
+
 import pandas as pd
+import numpy as np
 
+from glm_benchmarks.sklearn_fork import GeneralizedLinearRegressor
 
-# class SklearnForkBenchmark:
-#     def __init__(self, dat):
-#         dat = dat
+random_seed = 110
 
-    # def free(self):
-    #     return self.fit(alpha=0, l1_ratio=1)
+def runtime(f, *args, **kwargs):
+    start = time.time()
+    out = f(*args, **kwargs)
+    end = time.time()
+    return end - start, out
 
-    # def lasso(self):
-    #     return self.fit(alpha=0.01, l1_ratio=1)
+def build_and_fit(model_args, fit_args):
+    return GeneralizedLinearRegressor(**model_args).fit(**fit_args)
 
-    # def ridge(self):
-    #     return self.fit(alpha=0.01, l1_ratio=0)
+def sklearn_fork_bench(dat, distribution, alpha, l1_ratio):
+    result = dict()
 
-    # def net(self):
-    #     return self.fit(alpha=0.01, l1_ratio=0.5)
+    fit_args = dict(X=dat['X'], y=dat['y'], sample_weight=dat['exposure'])
+    model_args = dict(
+        family=distribution,
+        alpha=alpha,
+        l1_ratio=l1_ratio,
+        max_iter=10000,
+        random_state=random_seed,
+        copy_X=True,
+        tol=1e-3
+    )
+
+    result['runtime'], m = runtime(build_and_fit, model_args, fit_args)
+    result['model_obj'] = m
+    result['intercept'] = m.intercept_
+    result['coeffs'] = m.coef_
+    result['n_iter'] = m.n_iter_
+
+    result['path'] = compute_path(m.n_iter_, model_args, fit_args)
+    np.testing.assert_almost_equal(
+        result['path'][-1],
+        result['coeffs'],
+        -np.log10(model_args['tol']) - 1
+    )
+    return result
+
+def compute_path(niters, model_args, fit_args):
+    step_model_args = model_args.copy()
+    step_model_args['max_iter'] = 1
+    step_model_args['warm_start'] = True
+    m = GeneralizedLinearRegressor(**step_model_args)
+    path = []
+    for i in range(niters):
+        m.fit(**fit_args)
+        path.append(m.coef_.copy())
+    return path
