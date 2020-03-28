5e952a217b188d60272f734502f4c04f02fe6187
Ben Thompson <t.ben.thompson@gmail.com>
2020-03-28 11:15:39 -0400

    Add lint script and run all linters.

--- benchmarks/sklearn_fork.py
+++ benchmarks/sklearn_fork.py
@@ -1,11 +1,11 @@
 import time
 
-import pandas as pd
 import numpy as np
 
 from glm_benchmarks.sklearn_fork import GeneralizedLinearRegressor
 
 random_seed = 110
+
 
 def runtime(f, *args, **kwargs):
     start = time.time()
@@ -13,13 +13,15 @@ def runtime(f, *args, **kwargs):
     end = time.time()
     return end - start, out
 
+
 def build_and_fit(model_args, fit_args):
     return GeneralizedLinearRegressor(**model_args).fit(**fit_args)
+
 
 def sklearn_fork_bench(dat, distribution, alpha, l1_ratio):
     result = dict()
 
-    fit_args = dict(X=dat['X'], y=dat['y'], sample_weight=dat['exposure'])
+    fit_args = dict(X=dat["X"], y=dat["y"], sample_weight=dat["exposure"])
     model_args = dict(
         family=distribution,
         alpha=alpha,
@@ -27,30 +29,31 @@ def sklearn_fork_bench(dat, distribution, alpha, l1_ratio):
         max_iter=10000,
         random_state=random_seed,
         copy_X=True,
-        tol=1e-3
+        tol=1e-3,
     )
 
-    result['runtime'], m = runtime(build_and_fit, model_args, fit_args)
-    result['model_obj'] = m
-    result['intercept'] = m.intercept_
-    result['coeffs'] = m.coef_
-    result['n_iter'] = m.n_iter_
+    result["runtime"], m = runtime(build_and_fit, model_args, fit_args)
+    result["model_obj"] = m
+    result["intercept"] = m.intercept_
+    result["coeffs"] = m.coef_
+    result["n_iter"] = m.n_iter_
 
-    result['path'] = compute_path(m.n_iter_, model_args, fit_args)
+    result["path"] = compute_path(m.n_iter_, model_args, fit_args)
     np.testing.assert_almost_equal(
-        result['path'][-1],
-        result['coeffs'],
-        -np.log10(model_args['tol']) - 1
+        result["path"][-1], result["coeffs"], -np.log10(model_args["tol"]) - 1
     )
     return result
 
+
 def compute_path(niters, model_args, fit_args):
     step_model_args = model_args.copy()
-    step_model_args['max_iter'] = 1
-    step_model_args['warm_start'] = True
+    step_model_args["max_iter"] = 1
+    step_model_args["warm_start"] = True
     m = GeneralizedLinearRegressor(**step_model_args)
     path = []
     for i in range(niters):
+        start = time.time()
         m.fit(**fit_args)
+        print(time.time() - start)
         path.append(m.coef_.copy())
     return path
