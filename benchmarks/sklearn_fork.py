5cccffff7a7a7cb5492f7bd7070dc7d4ecccab04
Ben Thompson <t.ben.thompson@gmail.com>
2020-03-28 10:07:45 -0400

    Simple comparison of sklearn_fork against glmnet_python.

--- benchmarks/sklearn_fork.py
+++ benchmarks/sklearn_fork.py
@@ -1,35 +1,18 @@
 import pandas as pd
-from git_root import git_root
 
-from glm_benchmarks.sklearn_fork import GeneralizedLinearRegressor
 
-# get data
-df = pd.read_parquet(git_root("data/data.parquet"))
-import ipdb
-ipdb.set_trace()
-X = df[[col for col in df.columns if col not in ["y", "exposure"]]]
-y = df["y"]
-exposure = df["exposure"]
+# class SklearnForkBenchmark:
+#     def __init__(self, dat):
+#         dat = dat
 
-# inspect
-X.shape
+    # def free(self):
+    #     return self.fit(alpha=0, l1_ratio=1)
 
-# run poisson regression, no regularization
-model_free = GeneralizedLinearRegressor(family="poisson", alpha=0).fit(
-    X=X, y=y, sample_weight=exposure
-)
+    # def lasso(self):
+    #     return self.fit(alpha=0.01, l1_ratio=1)
 
-# run poisson regression, L1 regularization
-model_lasso = GeneralizedLinearRegressor(family="poisson", alpha=1, l1_ratio=1).fit(
-    X=X, y=y, sample_weight=exposure
-)
+    # def ridge(self):
+    #     return self.fit(alpha=0.01, l1_ratio=0)
 
-# run poisson regression, L2 regularization
-model_ridge = GeneralizedLinearRegressor(family="poisson", alpha=1, l1_ratio=0).fit(
-    X=X, y=y, sample_weight=exposure
-)
-
-# run poisson regression, L1 & L2 regularization
-model_elnet = GeneralizedLinearRegressor(family="poisson", alpha=1, l1_ratio=0.5).fit(
-    X=X, y=y, sample_weight=exposure
-)
+    # def net(self):
+    #     return self.fit(alpha=0.01, l1_ratio=0.5)
