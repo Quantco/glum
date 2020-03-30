a767bba9bbedf1ec4982bb9045ffeb4a1c541153
Ben Thompson <t.ben.thompson@gmail.com>
2020-03-30 12:09:44 -0400

    Separating into two CLI tools.

--- src/glm_benchmarks/main.py
+++ src/glm_benchmarks/main.py
@@ -1,60 +1,118 @@
-import git_root
+import os
+import pickle
+
+import click
 import numpy as np
-import pandas as pd
-from glmnet_python import glmnet
 
-from glm_benchmarks.bench_sklearn_fork import runtime, sklearn_fork_bench
-
-
-def load_data(nrows=None):
-    df = pd.read_parquet(git_root.git_root("data/data.parquet"))
-    if nrows is not None:
-        df = df.iloc[:nrows]
-    X = df[[col for col in df.columns if col not in ["y", "exposure"]]]
-    y = df["y"]
-    exposure = df["exposure"]
-    return dict(X=X, y=y, exposure=exposure)
+from glm_benchmarks.bench_glmnet_python import glmnet_python_bench
+from glm_benchmarks.bench_sklearn_fork import sklearn_fork_bench
+from glm_benchmarks.problems import get_all_problems
 
 
-def glmnet_python_bench(dat, distribution, alpha, l1_ratio):
-    result = dict()
-    result["runtime"], m = runtime(
-        glmnet,
-        x=dat["X"].values.copy(),
-        y=dat["y"].values.copy(),
-        weights=dat["exposure"].values,
-        family=distribution,
-        alpha=l1_ratio,
-        lambdau=np.array([alpha]),
-        standardize=False,
-        thresh=1e-7,
-    )
-    result["model_obj"] = m
-    result["intercept"] = m["a0"]
-    result["coeffs"] = m["beta"][:, 0]
-    result["n_iter"] = m["npasses"]
-    return result
+@click.command()
+@click.option(
+    "--problem_names",
+    default="",
+    help="Specify a comma-separated list of benchmark problems you want to run. Leaving this blank will default to running all problems.",
+)
+@click.option(
+    "--library_names",
+    default="",
+    help="Specify a comma-separated list of libaries to benchmark. Leaving this blank will default to running all problems.",
+)
+@click.option(
+    "--num_rows",
+    type=int,
+    help="Pass an integer number of rows. This is useful for testing and development. The default is to use the full dataset.",
+)
+@click.option(
+    "--output_dir",
+    default="benchmark_output",
+    help="The directory to store benchmarking output.",
+)
+def cli_run(problem_names, library_names, num_rows, output_dir):
+    problems, libraries = get_limited_problems_libraries(problem_names, library_names)
+
+    for Pn, P in problems.items():
+        for Ln, L in libraries.items():
+            print(f"running problem={Pn} library={Ln}")
+            dat = P.data_loader(num_rows=num_rows)
+            result = L(dat, P.distribution, P.regularization_strength, P.l1_ratio)
+            save_benchmark_results(output_dir, Pn, Ln, result)
 
 
-def main():
-    dat = load_data(nrows=None)
-    benchmarks = dict(
+@click.command()
+@click.option(
+    "--problem_names",
+    default="",
+    help="Specify a comma-separated list of benchmark problems you want to analyze. Leaving this blank will default to analyzing all problems.",
+)
+@click.option(
+    "--library_names",
+    default="",
+    help="Specify a comma-separated list of libaries to analyze. Leaving this blank will default to analyzing all problems.",
+)
+@click.option(
+    "--output_dir",
+    default="benchmark_output",
+    help="The directory where we load benchmarking output.",
+)
+def cli_analyze(problem_names, library_names, output_dir):
+    np.set_printoptions(precision=4, suppress=True)
+    problems, libraries = get_limited_problems_libraries(problem_names, library_names)
+
+    # TODO: support more than a pair of libraries?
+    # NOTE: might be better to leave this more ad-hoc until the dust settles a bit.
+
+    for Pn in problems:
+        print("")
+        print(f"for {Pn}")
+
+        results = dict()
+        for Ln in libraries:
+            results[Ln] = load_benchmark_results(output_dir, Pn, Ln)
+            print(Ln, "number of iterations", results[Ln]["n_iter"])
+            print(Ln, "runtime", results[Ln]["runtime"])
+            print(
+                Ln, "runtime per iter", results[Ln]["runtime"] / results[Ln]["n_iter"]
+            )
+
+        print(results["glmnet_python"]["coeffs"] - results["sklearn_fork"]["coeffs"])
+
+
+def get_limited_problems_libraries(problem_names, library_names):
+    all_problems = get_all_problems()
+    all_libraries = dict(
         sklearn_fork=sklearn_fork_bench, glmnet_python=glmnet_python_bench
     )
-    results = dict()
-    np.set_printoptions(precision=4, suppress=True)
-    for name, fnc in benchmarks.items():
-        results[name] = fnc(dat, "poisson", 0.001, 0.5)
-        print(results[name]["intercept"])
-        print(results[name]["coeffs"])
 
-    for k in results:
-        print(k, "number of iterations", results[k]["n_iter"])
-        print(k, "runtime", results[k]["runtime"])
-        print(k, "runtime per iter", results[k]["runtime"] / results[k]["n_iter"])
+    if len(problem_names) > 0:
+        problem_names_split = get_comma_sep_names(problem_names)
+        problems = {k: all_problems[k] for k in problem_names_split}
+    else:
+        problems = all_problems
 
-    print(results["glmnet_python"]["coeffs"] - results["sklearn_fork"]["coeffs"])
+    if len(problem_names) > 0:
+        library_names_split = get_comma_sep_names(library_names)
+        libraries = {k: all_libraries[k] for k in library_names_split}
+    else:
+        libraries = all_libraries
+    return problems, libraries
 
 
-if __name__ == "__main__":
-    main()
+def get_comma_sep_names(xs):
+    return [x.strip() for x in xs.split(",")]
+
+
+def save_benchmark_results(output_dir, problem_name, library_name, result):
+    problem_dir = os.path.join(output_dir, problem_name)
+    if not os.path.exists(problem_dir):
+        os.makedirs(problem_dir)
+    with open(os.path.join(problem_dir, library_name + "-results.pkl"), "wb") as f:
+        pickle.dump(result, f)
+
+
+def load_benchmark_results(output_dir, problem_name, library_name):
+    problem_dir = os.path.join(output_dir, problem_name)
+    with open(os.path.join(problem_dir, library_name + "-results.pkl"), "rb") as f:
+        return pickle.load(f)
