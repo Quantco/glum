bc2cbf55016f2a5959c5bda31047cdc0efd927d4
Jan Tilly <jantilly@gmail.com>
2020-03-31 19:39:40 +0200

    Add sparse and dense data set. Add option to upsample.

---
+++ src/glm_benchmarks/data/__init__.py
@@ -0,0 +1,11 @@
+from .create import (
+    create_raw_data,
+    generate_simple_insurance_dataset,
+    generate_sparse_insurance_dataset,
+)
+
+__all__ = [
+    "generate_simple_insurance_dataset",
+    "generate_sparse_insurance_dataset",
+    "create_raw_data",
+]
