ebda213117b8bab87adae2645e941d471062889b
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2020-03-31 15:40:48 -0400

    Exposure is not weight (#8)
    * Moved exposure to exposure; it had been used as a weight
    
    * Removed reference to exposure in sklearn_fork
    
    * Reverted towards older behavior because it was appropriate for exposures to be passed as weights. Renamed exposure as weights for clarity.
    
    Co-authored-by: Elizabeth Santorella <elizabeth.santorella@gmail.com>

--- src/glm_benchmarks/bench_glmnet_python.py
+++ src/glm_benchmarks/bench_glmnet_python.py
@@ -6,17 +6,20 @@ from .util import runtime
 
 def glmnet_python_bench(dat, distribution, alpha, l1_ratio):
     result = dict()
-    result["runtime"], m = runtime(
-        glmnet,
+    glmnet_kws = dict(
         x=dat["X"].values.copy(),
         y=dat["y"].values.copy(),
-        weights=dat["exposure"].values,
         family=distribution,
         alpha=l1_ratio,
         lambdau=np.array([alpha]),
         standardize=False,
         thresh=1e-7,
     )
+    if "weights" in dat.keys():
+        glmnet_kws.update({"weights": dat["weights"].values})
+
+    result["runtime"], m = runtime(glmnet, weights=dat["weights"].values, **glmnet_kws)
+
     result["model_obj"] = m
     result["intercept"] = m["a0"]
     result["coef"] = m["beta"][:, 0]
