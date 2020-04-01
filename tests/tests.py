089affac7c51ba409531e7eb6ed3f0c98aefdaa1
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2020-04-01 13:14:47 -0400

    Tensorflow sparse support

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,9 @@
+import numpy as np
+import tensorflow as tf
 from click.testing import CliRunner
+from scipy import sparse as sps
 
+from glm_benchmarks.bench_tensorflow import sps_to_tf_sparse
 from glm_benchmarks.main import cli_analyze, cli_run, get_limited_problems_libraries
 
 # Minimum number of rows for which all benchmarks will run
@@ -23,3 +27,11 @@ def test_cli_analyze__runs_default_kws():
     runner = CliRunner()
     result = runner.invoke(cli_analyze, [])
     assert result.exit_code == 0
+
+
+def test_sps_to_tf_sparse():
+    x = sps.eye(4)
+    tensor = sps_to_tf_sparse(x)
+    orig_as_dense = x.A
+    new_as_dense = tf.sparse.to_dense(tensor).numpy()
+    np.testing.assert_almost_equal(orig_as_dense, new_as_dense)
