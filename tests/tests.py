f4688518c2b47c8b5df12c371aad4dab1d24a7fa
Ben Thompson <t.ben.thompson@gmail.com>
2020-04-06 22:58:08 -0400

    Add sparse matrix benchmarking to h2o.

--- tests/tests.py
+++ tests/tests.py
@@ -4,6 +4,10 @@ from click.testing import CliRunner
 from scipy import sparse as sps
 
 from glm_benchmarks.bench_tensorflow import sps_to_tf_sparse
+from glm_benchmarks.data import (
+    generate_simple_insurance_dataset,
+    generate_sparse_insurance_dataset,
+)
 from glm_benchmarks.main import cli_analyze, cli_run, get_limited_problems_libraries
 
 # Minimum number of rows for which all benchmarks will run
@@ -35,3 +39,12 @@ def test_sps_to_tf_sparse():
     orig_as_dense = x.A
     new_as_dense = tf.sparse.to_dense(tensor).numpy()
     np.testing.assert_almost_equal(orig_as_dense, new_as_dense)
+
+
+def test_dense_and_sparse_matrices_equal():
+    dat_dense = generate_simple_insurance_dataset(100)
+    dat_sparse = generate_sparse_insurance_dataset(100)
+    # TODO: This currently fails. Is it intentional that the matrices do not match?
+    # np.testing.assert_almost_equal(dat_dense[0], dat_sparse[0].todense())
+    np.testing.assert_almost_equal(dat_dense[1], dat_sparse[1])
+    np.testing.assert_almost_equal(dat_dense[2], dat_sparse[2])
