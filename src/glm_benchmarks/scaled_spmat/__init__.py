842298699598a48f4b145c3d23c83ad035fbddf9
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2020-04-13 11:20:51 -0400

    Cleaning up code

---
+++ src/glm_benchmarks/scaled_spmat/__init__.py
@@ -0,0 +1,3 @@
+from .scaled_spmat import ColScaledSpMat, RowScaledSpMat
+
+__all__ = ["ColScaledSpMat", "RowScaledSpMat"]
--- src/glm_benchmarks/scaled_spmat/tests.py
+++ src/glm_benchmarks/scaled_spmat/tests.py
@@ -12,8 +12,7 @@ def row_scaled_mat() -> RowScaledSpMat:
     np.random.seed(0)
     sp_mat = sps.random(n_rows, n_cols, density=0.8)
     shift = np.random.uniform(0, 1, n_rows)
-    mult = np.random.uniform(0, 1, n_rows)
-    return RowScaledSpMat(sp_mat, shift, mult)
+    return RowScaledSpMat(sp_mat, shift)
 
 
 def col_scaled_mat() -> ColScaledSpMat:
@@ -23,8 +22,7 @@ def col_scaled_mat() -> ColScaledSpMat:
     np.random.seed(0)
     sp_mat = sps.random(n_rows, n_cols, density=0.8)
     col_shift = np.random.uniform(0, 1, n_cols)
-    col_mult = np.random.uniform(0, 1, n_cols)
-    return ColScaledSpMat(sp_mat, col_shift, col_mult)
+    return ColScaledSpMat(sp_mat, col_shift)
 
 
 @pytest.fixture
@@ -32,7 +30,12 @@ def col_scaled_mat_fixture():
     return col_scaled_mat()
 
 
-def test_setup_and_densify():
+@pytest.fixture
+def row_scaled_mat_fixture():
+    return row_scaled_mat()
+
+
+def test_setup_and_densify_col():
 
     n_rows = 4
     n_cols = 3
@@ -40,9 +43,8 @@ def test_setup_and_densify():
     np.random.seed(0)
     sp_mat = sps.random(n_rows, n_cols, density=0.8)
     col_shift = np.random.uniform(0, 1, n_cols)
-    col_mult = np.random.uniform(0, 1, n_cols)
-    col_scaled_mat = ColScaledSpMat(sp_mat, col_shift, col_mult)
-    expected = (sp_mat.A + col_shift[None, :]) * col_mult[None, :]
+    col_scaled_mat = ColScaledSpMat(sp_mat, col_shift)
+    expected = sp_mat.A + col_shift[None, :]
     assert col_scaled_mat.A.shape == (n_rows, n_cols)
     np.testing.assert_almost_equal(col_scaled_mat.A, expected)
 
@@ -55,9 +57,8 @@ def test_setup_and_densify_row():
     np.random.seed(0)
     sp_mat = sps.random(n_rows, n_cols, density=0.8)
     shift = np.random.uniform(0, 1, n_rows)
-    mult = np.random.uniform(0, 1, n_rows)
-    scaled_mat = RowScaledSpMat(sp_mat, shift, mult)
-    expected = (sp_mat.A + shift[:, None]) * mult[:, None]
+    scaled_mat = RowScaledSpMat(sp_mat, shift)
+    expected = sp_mat.A + shift[:, None]
     assert scaled_mat.A.shape == (n_rows, n_cols)
     np.testing.assert_almost_equal(scaled_mat.A, expected)
 
@@ -92,27 +93,55 @@ def test_col_dot_sparse_mat(scaled_mat_builder):
     np.testing.assert_almost_equal(result.A, expected)
 
 
-def test_power(col_scaled_mat_fixture: ColScaledSpMat):
+@pytest.mark.parametrize("scaled_mat_builder", [col_scaled_mat, row_scaled_mat])
+def test_power(scaled_mat_builder):
     p = 3
-    result = col_scaled_mat_fixture.power(p)
-    assert isinstance(result, ColScaledSpMat)
-    assert result.shape == col_scaled_mat_fixture.shape
-    np.testing.assert_almost_equal(result.A, col_scaled_mat_fixture.A ** p)
+    scaled_mat = scaled_mat_builder()
+    result = scaled_mat.power(p)
+    assert isinstance(result, type(scaled_mat))
+    assert result.shape == scaled_mat.shape
+    np.testing.assert_almost_equal(result.A, scaled_mat.A ** p)
 
 
-def test_transpose_smoke_test(col_scaled_mat_fixture: ColScaledSpMat):
+def test_transpose_type_col(col_scaled_mat_fixture: ColScaledSpMat):
     t = col_scaled_mat_fixture.T
     assert isinstance(t, RowScaledSpMat)
     assert t.shape == (col_scaled_mat_fixture.shape[1], col_scaled_mat_fixture.shape[0])
 
 
-def test_transpose_against_dense(col_scaled_mat_fixture: ColScaledSpMat):
+def test_transpose_type_row(row_scaled_mat_fixture: RowScaledSpMat):
+    t = row_scaled_mat_fixture.T
+    assert isinstance(t, ColScaledSpMat)
+    assert t.shape == (row_scaled_mat_fixture.shape[1], row_scaled_mat_fixture.shape[0])
+
+
+@pytest.mark.parametrize("scaled_mat_builder", [col_scaled_mat, row_scaled_mat])
+def test_transpose_against_dense(scaled_mat_builder):
+    scaled_mat = scaled_mat_builder()
+    np.testing.assert_almost_equal(scaled_mat.T.A, scaled_mat.A.T)
+
+
+@pytest.mark.parametrize("scaled_mat_builder", [col_scaled_mat, row_scaled_mat])
+def test_transpose_reversible(scaled_mat_builder):
+    scaled_mat = scaled_mat_builder()
+    two_trans = scaled_mat.T.T
+    assert (two_trans.mat != scaled_mat.mat).sum() == 0
+    assert (two_trans.shift == scaled_mat.shift).all()
+
+
+@pytest.mark.parametrize("scaled_mat_builder", [col_scaled_mat, row_scaled_mat])
+def test_multiply(scaled_mat_builder):
+    scaled_mat = scaled_mat_builder()
+    other = np.random.normal(0, 1, (scaled_mat.shape[scaled_mat.scale_axis]))
+    result = scaled_mat.multiply(other)
     np.testing.assert_almost_equal(
-        col_scaled_mat_fixture.T.A, col_scaled_mat_fixture.A.T
+        result.A, scaled_mat.A * np.expand_dims(other, 1 - scaled_mat.scale_axis)
     )
 
 
-def test_transpose_reversible(col_scaled_mat_fixture: ColScaledSpMat):
-    two_trans = col_scaled_mat_fixture.T.T
-    assert (two_trans.mat != col_scaled_mat_fixture.mat).sum() == 0
-    assert (two_trans.shift == col_scaled_mat_fixture.shift).all()
+@pytest.mark.parametrize("scaled_mat_builder", [col_scaled_mat, row_scaled_mat])
+def test_multiply_scalar(scaled_mat_builder):
+    other = 4.3
+    scaled_mat = scaled_mat_builder()
+    result = scaled_mat.multiply(other)
+    np.testing.assert_almost_equal(result.A, scaled_mat.A * other)
