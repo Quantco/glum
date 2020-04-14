659ab2fb780dc4c1e0e8967a4aabfccb01e9c7fc
Ben Thompson <t.ben.thompson@gmail.com>
2020-04-14 12:27:54 -0400

    simple golden master testing mechanism for profiling

--- src/glm_benchmarks/profile_entry.py
+++ src/glm_benchmarks/profile_entry.py
@@ -1,9 +1,11 @@
+import os
+import pickle
+
 import click
 import numpy as np
 
 from glm_benchmarks.bench_sklearn_fork import sklearn_fork_bench
-from glm_benchmarks.main import execute_problem_library
-from glm_benchmarks.problems import get_all_problems
+from glm_benchmarks.main import execute_problem_library, get_limited_problems
 
 intercept = -3.366495969747295
 coef = np.array(
@@ -74,13 +76,58 @@ coef = np.array(
     default=50000,
     help="Integer number of rows to run profiling on.",
 )
-def main(num_rows):
-    problems = get_all_problems()
-    Pn = "simple_insurance_no_weights_lasso_poisson"
-    result = execute_problem_library(problems[Pn], sklearn_fork_bench, num_rows)
-    if num_rows == 50000:
-        np.testing.assert_almost_equal(result["intercept"], intercept)
-        np.testing.assert_almost_equal(result["coef"], coef)
+@click.option(
+    "--problem_names",
+    default="simple_insurance_no_weights_lasso_poisson",
+    help="Specify a comma-separated list of benchmark problems you want to run.",
+)
+@click.option(
+    "--sparsify",
+    is_flag=True,
+    help="Convert an originally dense problem into a sparse one.",
+)
+@click.option(
+    "--save_result",
+    is_flag=True,
+    help="Save the estimates for later golden master testing.",
+)
+@click.option(
+    "--save_dir",
+    default="golden_master",
+    help="Where to find saved estimates for checking that estimates haven't changed.",
+)
+def main(num_rows, problem_names, sparsify, save_result, save_dir):
+    problems = get_limited_problems(problem_names)
+    for Pn in problems:
+        print(f"benchmarking {Pn}")
+        result = execute_problem_library(
+            problems[Pn], sklearn_fork_bench, num_rows, sparsify
+        )
+
+        path = os.path.join(save_dir, Pn, str(num_rows) + ".npy")
+        if save_result:
+            save_baseline(path, result)
+        else:
+            test_against_baseline(path, result)
+        print("")
+
+
+def save_baseline(path, data):
+    print("saving baseline estimates for later testing.")
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, "wb") as f:
+        pickle.dump(data, f)
+
+
+def test_against_baseline(path, data):
+    print("loading baseline estimates for testing")
+    with open(path, "rb") as f:
+        baseline = pickle.load(f)
+    np.testing.assert_almost_equal(data["intercept"], baseline["intercept"])
+    np.testing.assert_almost_equal(data["coef"], baseline["coef"])
+    print("test passed")
+    print(f"baseline runtime = {baseline['runtime']}")
+    print(f"current runtime = {data['runtime']}")
 
 
 if __name__ == "__main__":
