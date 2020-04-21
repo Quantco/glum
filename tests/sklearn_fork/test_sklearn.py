2ffe29c83f5daef180969abb352fa57839c1104e
Ben Thompson <t.ben.thompson@gmail.com>
2020-04-21 17:19:54 -0400

    Handling Liz's comments.

--- tests/sklearn_fork/test_sklearn.py
+++ tests/sklearn_fork/test_sklearn.py
@@ -491,52 +491,52 @@ def test_normal_ridge_comparison(n_samples, n_features, fit_intercept, solver):
 
     Compare to test_ridge in test_ridge.py.
     """
-    for i in range(10):
-        alpha = 1.0
-        n_predict = 10
-        X, y, coef = make_regression(
-            n_samples=n_samples + n_predict,
-            n_features=n_features,
-            n_informative=n_features - 2,
-            noise=0.5,
-            coef=True,
-        )
-        y = y[0:n_samples]
-        X, T = X[0:n_samples], X[n_samples:]
+    alpha = 1.0
+    n_predict = 10
+    X, y, coef = make_regression(
+        n_samples=n_samples + n_predict,
+        n_features=n_features,
+        n_informative=n_features - 2,
+        noise=0.5,
+        coef=True,
+    )
+    y = y[0:n_samples]
+    X, T = X[0:n_samples], X[n_samples:]
 
-        if n_samples > n_features:
-            ridge_params = {"solver": "svd"}
-        else:
-            ridge_params = {"solver": "sag", "max_iter": 10000, "tol": 1e-9}
+    if n_samples > n_features:
+        ridge_params = {"solver": "svd"}
+    else:
+        ridge_params = {"solver": "sag", "max_iter": 10000, "tol": 1e-9}
 
-        # GLM has 1/(2*n) * Loss + 1/2*L2, Ridge has Loss + L2
-        ridge = Ridge(alpha=alpha * n_samples, normalize=False, **ridge_params)
-        ridge.fit(X, y)
+    # GLM has 1/(2*n) * Loss + 1/2*L2, Ridge has Loss + L2
+    ridge = Ridge(alpha=alpha * n_samples, normalize=False, **ridge_params)
+    ridge.fit(X, y)
 
-        glm = GeneralizedLinearRegressor(
-            alpha=1.0,
-            l1_ratio=0,
-            family="normal",
-            link="identity",
-            fit_intercept=True,
-            max_iter=300,
-            solver=solver,
-            tol=1e-6,
-            check_input=False,
-        )
-        glm.fit(X, y)
-        assert glm.coef_.shape == (X.shape[1],)
-        assert_allclose(glm.coef_, ridge.coef_, rtol=5e-5)
-        assert_allclose(glm.intercept_, ridge.intercept_, rtol=1e-5)
-        assert_allclose(glm.predict(T), ridge.predict(T), rtol=1e-4)
+    glm = GeneralizedLinearRegressor(
+        alpha=1.0,
+        l1_ratio=0,
+        family="normal",
+        link="identity",
+        fit_intercept=True,
+        max_iter=300,
+        solver=solver,
+        tol=1e-6,
+        check_input=False,
+    )
+    glm.fit(X, y)
+    assert glm.coef_.shape == (X.shape[1],)
+    assert_allclose(glm.coef_, ridge.coef_, rtol=5e-4)
+    assert_allclose(glm.intercept_, ridge.intercept_, rtol=1e-4)
+    assert_allclose(glm.predict(T), ridge.predict(T), rtol=1e-3)
 
 
 @pytest.mark.parametrize(
     "solver, tol", [("irls", 1e-7), ("lbfgs", 1e-7), ("newton-cg", 1e-7), ("cd", 1e-7)]
 )
 @pytest.mark.parametrize("center_predictors", [True, False])
+@pytest.mark.parametrize("scale_predictors", [True, False])
 @pytest.mark.parametrize("use_sparse", [True, False])
-def test_poisson_ridge(solver, tol, center_predictors, use_sparse):
+def test_poisson_ridge(solver, tol, center_predictors, scale_predictors, use_sparse):
     """Test ridge regression with poisson family and LogLink.
 
     Compare to R's glmnet"""
@@ -551,10 +551,28 @@ def test_poisson_ridge(solver, tol, center_predictors, use_sparse):
     # (Intercept) -0.12889386979
     # a            0.29019207995
     # b            0.03741173122
-    X = np.array([[-2, -1, 1, 2], [0, 0, 1, 1]]).T
+    #
+    # fit <- glmnet(x=x, y=y, alpha=0, intercept=T, family="poisson",
+    #               standardize=T, thresh=1e-10, nlambda=10000)
+    # coef(fit, s=1)
+    # (Intercept) -0.21002571120839675
+    # a            0.16472093,
+    # b            0.27051971
+
+    # Alternately, for running from Python:
+    # from glmnet_python import glmnet
+    # model = glmnet(x=X_dense, y=y, alpha=0, family="poisson",
+    #               standardize=scale_predictors, thresh=1e-10, lambdau=np.array([1.0]))
+    # true_intercept = model["a0"][0]
+    # true_beta = model["beta"][:, 0]
+    # print(true_intercept, true_beta)
+
+    X_dense = np.array([[-2, -1, 1, 2], [0, 0, 1, 1]], dtype=np.float).T
     if use_sparse:
-        X = sparse.csc_matrix(X)
-    y = np.array([0, 1, 1, 2])
+        X = sparse.csc_matrix(X_dense)
+    else:
+        X = X_dense
+    y = np.array([0, 1, 1, 2], dtype=np.float)
     rng = np.random.RandomState(42)
     glm = GeneralizedLinearRegressor(
         alpha=1,
@@ -568,13 +586,25 @@ def test_poisson_ridge(solver, tol, center_predictors, use_sparse):
         random_state=rng,
         copy_X=True,
         center_predictors=center_predictors,
+        scale_predictors=scale_predictors,
     )
+
+    if not center_predictors and scale_predictors:
+        with pytest.raises(ValueError):
+            glm.fit(X, y)
+        return
+
     glm2 = copy.deepcopy(glm)
 
     def check(G):
         G.fit(X, y)
-        assert_allclose(G.intercept_, -0.12889386979, rtol=1e-5)
-        assert_allclose(G.coef_, [0.29019207995, 0.03741173122], rtol=1e-5)
+        if scale_predictors:
+            print(G.intercept_, G.coef_)
+            assert_allclose(G.intercept_, -0.21002571120839675, rtol=1e-5)
+            assert_allclose(G.coef_, [0.16472093, 0.27051971], rtol=1e-5)
+        else:
+            assert_allclose(G.intercept_, -0.12889386979, rtol=1e-5)
+            assert_allclose(G.coef_, [0.29019207995, 0.03741173122], rtol=1e-5)
 
     check(glm)
 
@@ -856,7 +886,7 @@ def test_standardize(use_sparse, scale_predictors):
     if use_sparse:
         M = sparse.csc_matrix(M)
 
-    X, col_means, col_stds = _standardize(M, np.ones(NR) / NR, True, scale_predictors)
+    X, col_means, col_stds = _standardize(M, np.ones(NR) / NR, scale_predictors)
     np.testing.assert_almost_equal(col_means[0, :], col_mults)
 
     # After standardization, all the columns will have the same values.
