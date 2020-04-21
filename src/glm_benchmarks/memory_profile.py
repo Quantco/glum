dba97b96d3ada57344461de4e833c94c26d080f2
Ben Thompson <t.ben.thompson@gmail.com>
2020-04-21 13:57:44 -0400

    Checking sparse matrices.

--- src/glm_benchmarks/memory_profile.py
+++ src/glm_benchmarks/memory_profile.py
@@ -1,6 +1,7 @@
 import pickle
 
 import click
+import scipy.sparse
 
 from glm_benchmarks.bench_sklearn_fork import sklearn_fork_bench
 from glm_benchmarks.main import get_limited_problems
@@ -19,9 +20,14 @@ from glm_benchmarks.main import get_limited_problems
     help="Specify a single benchmark problem you want to run.",
 )
 @click.option(
+    "--sparsify",
+    is_flag=True,
+    help="Convert an originally dense problem into a sparse one.",
+)
+@click.option(
     "--save", is_flag=True, help="Create the benchmark data, don't run the benchmark."
 )
-def main(num_rows, problem_name, save):
+def main(num_rows, problem_name, sparsify, save):
     problems = get_limited_problems(problem_name)
     P = list(problems.values())[0]
     save_path = "memory_benchmark.pkl"
@@ -30,6 +36,8 @@ def main(num_rows, problem_name, save):
         # GLM functionality rather than also including the data creation in the
         # benchmark.
         dat = P.data_loader(num_rows)
+        if sparsify:
+            dat["X"] = scipy.sparse.csc_matrix(dat["X"])
         with open(save_path, "wb") as f:
             pickle.dump(dat, f)
     else:
@@ -39,6 +47,13 @@ def main(num_rows, problem_name, save):
             dat, P.distribution, P.regularization_strength, P.l1_ratio
         )
         print(f"took {result['runtime']}")
+
+        # import numpy as np
+        # d = np.random.rand(num_rows)
+        # from glm_benchmarks.scaled_spmat.standardize import zero_center
+        # from glm_benchmarks.sklearn_fork._glm import _safe_sandwich_dot
+        # dat['X'], means = zero_center(dat['X'])
+        # _safe_sandwich_dot(dat['X'], d, intercept = True)
 
 
 if __name__ == "__main__":
