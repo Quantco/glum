0518b51135edfd89be056c1c460ea934da4c3b14
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2020-04-22 19:15:04 -0400

    Bethompson fast matmul (#66)
    * Playing with a cythonized sandwich dot.
    
    * Ignoring auto-generated C file from Cython.
    
    * towards sparse blas
    
    * changing setup.py
    
    * Seems to work
    
    * two matrix multiplications
    
    * dynamic setup.py
    
    * small typo
    
    * back to standardization
    
    * using setup.py instead of pip to install
    
    * Added mkl_sparse_d_syrkd
    
    * [WIP] Liz benchmark sandwich (#61)
    
    * Update _glm.py
    
    * Update _glm.py
    
    * Black fix.
    
    * Remove print.
    
    * Make external packages optional.
    
    * Ignore parquet files unless add with git add -f.
    
    * Add real data to problems.
    
    * Add function for exposure correction.
    
    * Fix type.
    
    * Raise error if distribution is not poisson.
    
    * Set standardize=True. (#55)
    
    * glmnet-python needs sparse matrices in csc format. (#54)
    
    * Remove unneeded packages (#56)
    
    * Got rid of irrelevant libraries; fewer iterations for sklearn for testing; don't standardize zero-variance columns; error handleng for glmnet_qc; better reporting of sklearn_fork diagnostics
    
    * Fixed benchmarks so they actually run
    
    Co-authored-by: Elizabeth Santorella <elizabeth.santorella@gmail.com>
    
    * Changed dockerfile so it runs (need to manually run setup.py after; added sandwich benchmark
    
    Co-authored-by: Ben Thompson <t.ben.thompson@gmail.com>
    Co-authored-by: Jan Tilly <jan.tilly@quantco.com>
    Co-authored-by: Jan Tilly <jantilly@gmail.com>
    Co-authored-by: Elizabeth Santorella <elizabeth.santorella@gmail.com>
    
    * added _safe_sandwich_dot_whitened
    
    * Avoid copying in CSC _safe_sandwich_dot.
    
    * custom Cython (X.T * d) @ X implementation.
    
    * fast matmul2
    
    * compile inside the conda env.
    
    * Need to build at runtime.
    
    * Update Dockerfile
    
    Co-Authored-By: Jan Tilly <jantilly@gmail.com>
    
    * Cleanup
    
    Co-authored-by: Ben Thompson <t.ben.thompson@gmail.com>
    Co-authored-by: Marc-Antoine Schmidt <mschmidt@wayfair.com>
    Co-authored-by: Jan Tilly <jan.tilly@quantco.com>
    Co-authored-by: Jan Tilly <jantilly@gmail.com>
    Co-authored-by: Elizabeth Santorella <elizabeth.santorella@gmail.com>

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 from os import path
 
-from setuptools import find_packages, setup
+import numpy as np
+from Cython.Build import cythonize
+from setuptools import Extension, find_packages, setup
 
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, "README.md")) as f:
     long_description = f.read()
+
+include_mkl = np.__config__.get_info("blas_mkl_info")["include_dirs"][0]
+libraries_mkl = np.__config__.get_info("blas_mkl_info")["include_dirs"][0]
+
+ext_modules = [
+    Extension(
+        name="glm_benchmarks.spblas.mkl_spblas",
+        sources=["src/glm_benchmarks/spblas/mkl_spblas.pyx"],
+        include_dirs=[np.get_include(), include_mkl],
+        libraries=["mkl_rt"],
+        library_dirs=["", libraries_mkl],
+    ),
+]
 
 setup(
     name="glm_benchmarks",
@@ -30,5 +45,6 @@ setup(
         glm_benchmarks_run = glm_benchmarks.main:cli_run
         glm_benchmarks_analyze = glm_benchmarks.main:cli_analyze
     """,
-    console_scripts={},
+    ext_modules=cythonize(ext_modules, annotate=False),
+    zip_safe=False,
 )
