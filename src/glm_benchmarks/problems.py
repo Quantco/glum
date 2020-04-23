2ae539d04af53448572798788774941b0c440d3a
Jan Tilly <jantilly@gmail.com>
2020-04-23 20:22:45 +0000

    Rename data sets to narrow, wide, and real. Add storage option that can be either dense or sparse.

--- src/glm_benchmarks/problems.py
+++ src/glm_benchmarks/problems.py
@@ -1,30 +1,27 @@
 from functools import partial
 from os.path import isfile
-from typing import Callable, Dict, Optional, Union
+from typing import Callable, Dict, Optional
 
 import attr
 import numpy as np
 from git_root import git_root
-from scipy import sparse as sps
 
 from .data import (
-    generate_real_dense_insurance_dataset,
-    generate_simple_insurance_dataset,
-    generate_sparse_insurance_dataset,
+    generate_narrow_insurance_dataset,
+    generate_real_insurance_dataset,
+    generate_wide_insurance_dataset,
 )
 
 
 @attr.s
 class Problem:
-    data_loader = attr.ib(
-        type=Callable[[Optional[int]], Dict[str, Union[np.ndarray, sps.spmatrix]]]
-    )
+    data_loader = attr.ib(type=Callable[[Optional[int]], Dict[str, np.ndarray]])
     distribution = attr.ib(type=str)
     regularization_strength = attr.ib(type=float)
     l1_ratio = attr.ib(type=float)
 
 
-def load_simple_insurance_data(
+def load_narrow_insurance_data(
     num_rows: int = None, noise: float = None, distribution: str = "poisson",
 ) -> Dict[str, np.ndarray]:
     """
@@ -32,37 +29,18 @@ def load_simple_insurance_data(
     is appropriate to pass what is modeled as an 'exposure' as a weight. Everywhere else,
     exposures will be referred to as weights.
     """
-    X, y, exposure = generate_simple_insurance_dataset(num_rows, noise, distribution)
+    X, y, exposure = generate_narrow_insurance_dataset(num_rows, noise, distribution)
     return dict(X=X, y=y, weights=exposure)
 
 
-def load_simple_insurance_data_no_weights(
+def load_narrow_insurance_data_no_weights(
     num_rows: int = None, noise: float = None, distribution: str = "poisson",
 ) -> Dict[str, np.ndarray]:
-    X, y, _ = generate_simple_insurance_dataset(num_rows, noise, distribution)
+    X, y, _ = generate_narrow_insurance_dataset(num_rows, noise, distribution)
     return dict(X=X, y=y)
 
 
-def load_sparse_insurance_data(
-    num_rows: int = None, noise: float = None, distribution: str = "poisson",
-) -> Dict[str, Union[np.ndarray, sps.spmatrix]]:
-    """
-    Due to the way we have set up this problem, by rescaling the target variable, it
-    is appropriate to pass what is modeled as an 'exposure' as a weight. Everywhere else,
-    exposures will be referred to as weights.
-    """
-    X, y, exposure = generate_sparse_insurance_dataset(num_rows, noise, distribution)
-    return dict(X=X, y=y, weights=exposure)
-
-
-def load_sparse_insurance_data_no_weights(
-    num_rows: int = None, noise: float = None, distribution: str = "poisson",
-) -> Dict[str, Union[np.ndarray, sps.spmatrix]]:
-    X, y, _ = generate_sparse_insurance_dataset(num_rows, noise, distribution)
-    return dict(X=X, y=y)
-
-
-def load_real_dense_insurance_data(
+def load_wide_insurance_data(
     num_rows: int = None, noise: float = None, distribution: str = "poisson",
 ) -> Dict[str, np.ndarray]:
     """
@@ -70,24 +48,27 @@ def load_real_dense_insurance_data(
     is appropriate to pass what is modeled as an 'exposure' as a weight. Everywhere else,
     exposures will be referred to as weights.
     """
-    X, y, exposure = generate_real_dense_insurance_dataset(
-        num_rows, noise, distribution
-    )
+    X, y, exposure = generate_wide_insurance_dataset(num_rows, noise, distribution)
     return dict(X=X, y=y, weights=exposure)
 
 
-def load_real_sparse_insurance_data(
+def load_wide_insurance_data_no_weights(
     num_rows: int = None, noise: float = None, distribution: str = "poisson",
-) -> Dict[str, Union[np.ndarray, sps.spmatrix]]:
+) -> Dict[str, np.ndarray]:
+    X, y, _ = generate_wide_insurance_dataset(num_rows, noise, distribution)
+    return dict(X=X, y=y)
+
+
+def load_real_insurance_data(
+    num_rows: int = None, noise: float = None, distribution: str = "poisson",
+) -> Dict[str, np.ndarray]:
     """
     Due to the way we have set up this problem, by rescaling the target variable, it
     is appropriate to pass what is modeled as an 'exposure' as a weight. Everywhere else,
     exposures will be referred to as weights.
     """
-    X, y, exposure = generate_real_dense_insurance_dataset(
-        num_rows, noise, distribution
-    )
-    return dict(X=sps.csc_matrix(X), y=y, weights=exposure)
+    X, y, exposure = generate_real_insurance_dataset(num_rows, noise, distribution)
+    return dict(X=X, y=y, weights=exposure)
 
 
 def get_all_problems() -> Dict[str, Problem]:
@@ -102,36 +83,36 @@ def get_all_problems() -> Dict[str, Problem]:
             if "tweedie" in data_version:
                 data_version = "tweedie"
 
-            problems["simple_insurance_" + suffix] = Problem(
+            problems["narrow_insurance_" + suffix] = Problem(
                 data_loader=partial(
-                    load_simple_insurance_data, distribution=data_version
+                    load_narrow_insurance_data, distribution=data_version
                 ),
                 distribution=distribution,
                 regularization_strength=regularization_strength,
                 l1_ratio=l1_ratio,
             )
 
-            problems["simple_insurance_no_weights_" + suffix] = Problem(
+            problems["narrow_insurance_no_weights_" + suffix] = Problem(
                 data_loader=partial(
-                    load_simple_insurance_data_no_weights, distribution=data_version
+                    load_narrow_insurance_data_no_weights, distribution=data_version
                 ),
                 distribution=distribution,
                 regularization_strength=regularization_strength,
                 l1_ratio=l1_ratio,
             )
 
-            problems["sparse_insurance_" + suffix] = Problem(
+            problems["wide_insurance_" + suffix] = Problem(
                 data_loader=partial(
-                    load_sparse_insurance_data, distribution=data_version
+                    load_wide_insurance_data, distribution=data_version
                 ),
                 distribution=distribution,
                 regularization_strength=regularization_strength,
                 l1_ratio=l1_ratio,
             )
 
-            problems["sparse_insurance_no_weights_" + suffix] = Problem(
+            problems["wide_insurance_no_weights_" + suffix] = Problem(
                 data_loader=partial(
-                    load_sparse_insurance_data_no_weights, distribution=data_version
+                    load_wide_insurance_data_no_weights, distribution=data_version
                 ),
                 distribution=distribution,
                 regularization_strength=regularization_strength,
@@ -140,18 +121,9 @@ def get_all_problems() -> Dict[str, Problem]:
 
             # only add these problems if you have the data
             if isfile(git_root("data", "X.parquet")):
-                problems["real_dense_insurance_" + suffix] = Problem(
+                problems["real_insurance_" + suffix] = Problem(
                     data_loader=partial(
-                        load_real_dense_insurance_data, distribution=data_version
-                    ),
-                    distribution=distribution,
-                    regularization_strength=regularization_strength,
-                    l1_ratio=l1_ratio,
-                )
-
-                problems["real_sparse_insurance_" + suffix] = Problem(
-                    data_loader=partial(
-                        load_real_sparse_insurance_data, distribution=data_version
+                        load_real_insurance_data, distribution=data_version
                     ),
                     distribution=distribution,
                     regularization_strength=regularization_strength,
