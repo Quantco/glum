e01513a8b479a5721d06aa5011bfa491913c5927
Ben Thompson <t.ben.thompson@gmail.com>
2020-04-28 10:36:38 -0400

    Unified Matrix API, MKL-based sparse matrix-vector products, Numexpr for LogLink and Poisson variance. (#86)
    * Playing with a cythonized sandwich dot.
    
    * Ignoring auto-generated C file from Cython.
    
    * towards sparse blas
    
    * changing setup.py
    
    * Seems to work
    
    * two matrix multiplications
    
    * dynamic setup.py
    
    * small typo
    
    * back to standardization
    
    * using setup.py instead of pip to install
    
    * Added mkl_sparse_d_syrkd
    
    * [WIP] Liz benchmark sandwich (#61)
    
    * Update _glm.py
    
    * Update _glm.py
    
    * Black fix.
    
    * Remove print.
    
    * Make external packages optional.
    
    * Ignore parquet files unless add with git add -f.
    
    * Add real data to problems.
    
    * Add function for exposure correction.
    
    * Fix type.
    
    * Raise error if distribution is not poisson.
    
    * Set standardize=True. (#55)
    
    * glmnet-python needs sparse matrices in csc format. (#54)
    
    * Remove unneeded packages (#56)
    
    * Got rid of irrelevant libraries; fewer iterations for sklearn for testing; don't standardize zero-variance columns; error handleng for glmnet_qc; better reporting of sklearn_fork diagnostics
    
    * Fixed benchmarks so they actually run
    
    Co-authored-by: Elizabeth Santorella <elizabeth.santorella@gmail.com>
    
    * Changed dockerfile so it runs (need to manually run setup.py after; added sandwich benchmark
    
    Co-authored-by: Ben Thompson <t.ben.thompson@gmail.com>
    Co-authored-by: Jan Tilly <jan.tilly@axa.de>
    Co-authored-by: Jan Tilly <jantilly@gmail.com>
    Co-authored-by: Elizabeth Santorella <elizabeth.santorella@gmail.com>
    
    * added _safe_sandwich_dot_whitened
    
    * Avoid copying in CSC _safe_sandwich_dot.
    
    * custom Cython (X.T * d) @ X implementation.
    
    * fast matmul2
    
    * compile inside the conda env.
    
    * Need to build at runtime.
    
    * Update Dockerfile
    
    Co-Authored-By: Jan Tilly <jantilly@gmail.com>
    
    * Starting
    
    * slower for now.
    
    * dense sandwich winning
    
    * refactor
    
    * recursive parallel dense sandwich product.
    
    * mako templating for C code.
    
    * trying to do better on matrices with large column count.
    
    * avx sparse sandwich, no good
    
    * forgot sparse-tmpl.c
    
    * 60% faster sparse sandwich.
    
    * Fixing up profile_entry.py
    
    * Fixed failing tests. And fixed docker image build.
    
    * Fixing dense sandiwch benchmark.
    
    * MKL sparse matrix-vector products.
    
    * Numexpr
    
    * 32-bit sparse sandwich, a little faster.
    
    * now conforming to normal sparse matrix apis.
    
    * tests passing, using sparse_dot package, fixed standardization
    
    * remove mkl compilation stuff from setup.py, moved DenseGLMDataMatrix to its own file.
    
    * Update conda-requirements.txt
    
    * Add numexpr to conda requirements.
    
    * Responding to suggestions from PR
    
    * benchmark error handling
    
    Co-authored-by: Marc-Antoine Schmidt <mschmidt@wayfair.com>
    Co-authored-by: ElizabethSantorellaQC <55885047+ElizabethSantorellaQC@users.noreply.github.com>
    Co-authored-by: Jan Tilly <jan.tilly@axa.de>
    Co-authored-by: Jan Tilly <jantilly@gmail.com>
    Co-authored-by: Elizabeth Santorella <elizabeth.santorella@gmail.com>

--- src/glm_benchmarks/scaled_spmat/__init__.py
+++ src/glm_benchmarks/scaled_spmat/__init__.py
@@ -1,4 +1,11 @@
+from .mkl_sparse_matrix import MKLSparseMatrix
 from .scaled_spmat import ColScaledSpMat, RowScaledSpMat
 from .standardize import standardize, zero_center
 
-__all__ = ["ColScaledSpMat", "RowScaledSpMat", "zero_center", "standardize"]
+__all__ = [
+    "ColScaledSpMat",
+    "RowScaledSpMat",
+    "zero_center",
+    "standardize",
+    "MKLSparseMatrix",
+]
