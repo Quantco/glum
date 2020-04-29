9c71027bac575160a0881093c9f3ea9292247416
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2020-04-29 10:57:20 -0400

    Added test for 32-bit float, currently failing

--- tests/sklearn_fork/test_fast_sandwich.py
+++ tests/sklearn_fork/test_fast_sandwich.py
@@ -1,13 +1,15 @@
 import numpy as np
+import pytest
 import scipy as sp
 import scipy.sparse
 
 from glm_benchmarks.sandwich.sandwich import dense_sandwich, sparse_sandwich
 
 
-def test_fast_sandwich():
-    shape = np.array([100, 50])
-    A = simulate_matrix(shape=shape, seed=0).tocsc()
+@pytest.mark.parametrize("dtype", [np.float64, np.float32])
+def test_fast_sandwich(dtype):
+    shape = (100, 50)
+    A = simulate_matrix(shape=shape, dtype=dtype).tocsc()
 
     d = np.ones(shape[0])
     true = A.T.dot(A).toarray()
@@ -19,11 +21,13 @@ def test_fast_sandwich():
     np.testing.assert_almost_equal(true, out2)
 
 
-def simulate_matrix(nonzero_frac=0.05, shape=[1000, 500], seed=0):
+def simulate_matrix(nonzero_frac=0.05, shape=(1000, 500), seed=0, dtype=np.float64):
 
     np.random.seed(seed)
     nnz = int(np.prod(shape) * nonzero_frac)
     row_index = np.random.randint(shape[0], size=nnz)
     col_index = np.random.randint(shape[1], size=nnz)
-    A = sp.sparse.csr_matrix((np.random.randn(nnz), (row_index, col_index)), shape)
+    A = sp.sparse.csr_matrix(
+        (np.random.randn(nnz).astype(dtype), (row_index, col_index)), shape
+    )
     return A
