2ab90614b061401d0dc15863c6ad4b1c5a3daea2
Ben Thompson <t.ben.thompson@gmail.com>
2020-04-30 15:41:38 -0400

    blocked inner avx. slow bc we have to rearrange before loading.

--- src/glm_benchmarks/benchmark_sparse_sandwich.py
+++ src/glm_benchmarks/benchmark_sparse_sandwich.py
@@ -6,7 +6,11 @@ import pandas as pd
 from scipy import sparse as sps
 
 from glm_benchmarks.problems import load_narrow_insurance_data
-from glm_benchmarks.sandwich.sandwich import sparse_sandwich
+from glm_benchmarks.sandwich.sandwich import (
+    dense_sandwich,
+    sparse_dense_sandwich,
+    sparse_sandwich,
+)
 
 
 def load_data(n_rows: int) -> Tuple[Any, np.ndarray]:
@@ -24,12 +28,61 @@ def _fast_sandwich(X, d):
     return sparse_sandwich(X, X.XT, d)
 
 
+def split(X):
+    # TODO: this splitting function is super inefficient. easy to optimize though...
+    densities = (X.indptr[1:] - X.indptr[:-1]) / X.shape[0]
+    sorted_indices = np.argsort(densities)[::-1]
+    sorted_densities = densities[sorted_indices]
+
+    threshold_density = 0.1
+    dense_indices = sorted_indices[sorted_densities > threshold_density]
+    sparse_indices = np.setdiff1d(sorted_indices, dense_indices)
+
+    X_dense_C = X.toarray()[:, dense_indices].copy()
+    X_dense = np.asfortranarray(X_dense_C)
+    X_sparse = sps.csc_matrix(X.toarray()[:, sparse_indices])
+    X_sparse_csr = X_sparse.tocsr()
+
+    def f(d):
+        SS = sparse_sandwich(X_sparse, X_sparse_csr, d)
+        DD = dense_sandwich(X_dense, d)
+        # DS2 = dot_product_mkl(X_sparse.T, d[:,np.newaxis] * X_dense)
+        DS = sparse_dense_sandwich(X_sparse, X_dense_C, d)
+        # DS2 = sparse_dense_sandwich(X_sparse_csr, X_dense, d)
+        # X_sparse_csr.data *= d[X_sparse_csr.indices]
+        # DS3 = dot_product_mkl(X_sparse_csr.T, X_dense)
+        # X_sparse_csr.data /= d[X_sparse_csr.indices]
+        # np.testing.assert_almost_equal(DS2, DS)
+
+        out = np.empty((X.shape[1], X.shape[1]))
+        out[np.ix_(dense_indices, dense_indices)] = DD
+        out[np.ix_(sparse_indices, sparse_indices)] = SS
+        out[np.ix_(sparse_indices, dense_indices)] = DS
+        out[np.ix_(dense_indices, sparse_indices)] = DS.T
+
+        return out
+
+    X.split_sandwich = f
+
+
+def split_sandwich(X, d):
+    return X.split_sandwich(d)
+
+
+def _dense_sandwich(X, d):
+    return dense_sandwich(X.X_dense, d)
+
+
 def run_one_problem_all_methods(x, d, include_naive, dtype) -> pd.DataFrame:
     x = x.astype(dtype)
     d = d.astype(dtype)
     x.XT = x.T.tocsc()
+    split(x)
+    x.X_dense = np.asfortranarray(x.toarray())
     funcs: Dict[str, Callable[[Any, np.ndarray], Any]] = {
         "fast_sandwich": _fast_sandwich,
+        "dense_sandwich": _dense_sandwich,
+        "split_sandwich": split_sandwich,
     }
     if include_naive:
         funcs["naive"] = naive_sandwich
@@ -37,7 +90,7 @@ def run_one_problem_all_methods(x, d, include_naive, dtype) -> pd.DataFrame:
     info: Dict[str, Any] = {}
     for name, func in funcs.items():
         ts = []
-        for i in range(10):
+        for i in range(3):
             start = time.perf_counter()
             res = func(x, d)
             ts.append(time.perf_counter() - start)
@@ -49,7 +102,7 @@ def run_one_problem_all_methods(x, d, include_naive, dtype) -> pd.DataFrame:
 
     if include_naive:
         naive_result = info["naive"]["res"]
-        for k in info.keys():
+        for k in info:
             np.testing.assert_allclose(naive_result, info[k]["res"], 4)
 
     return pd.DataFrame(
@@ -61,16 +114,17 @@ def main() -> None:
     # "killed" with 1e7 and 4 * 1e6
     row_counts = [
         int(1e4),
-        int(1e5),
+        # int(1e5),
         int(3e5),
         int(1e6),
+        int(2e6),
     ]  # , int(2e6), int(4e6), int(10e6)]
     benchmarks = []
 
     x, d = load_data(row_counts[-1])
 
     for i, n_rows in enumerate(row_counts):
-        for dtype in [np.float32, np.float64]:
+        for dtype in [np.float64]:
             benchmarks.append(
                 run_one_problem_all_methods(
                     x[:n_rows, :].copy(), d[:n_rows].copy(), i == 0, dtype
--- src/glm_benchmarks/sandwich/sandwich.pyx
+++ src/glm_benchmarks/sandwich/sandwich.pyx
@@ -1,4 +1,4 @@
-# distutils: extra_compile_args=-fopenmp -O3 -ffast-math -march=native -msse -msse2 -mavx
+# distutils: extra_compile_args=-fopenmp -O3 -ffast-math -march=native
 # distutils: extra_link_args=-fopenmp
 import numpy as np
 
@@ -55,9 +55,66 @@ def sparse_sandwich(A, AT, floating[:] d):
     out += np.tril(out, -1).T
     return out
 
-
 cdef extern from "dense.c":
     void _dense_sandwich(double*, double*, double*, int, int) nogil
+    void _sparse_dense_sandwich(double*, int*, int*, double*, double*, double*, int, int, int) nogil
+
+def sparse_dense_sandwich(A, double[:,:] B, double[:] d):
+    # computes where (A.T * d) @ B
+    # assumes that A is in csr form
+    cdef double[:] Adata = A.data
+    cdef int[:] Aindices = A.indices
+    cdef int[:] Aindptr = A.indptr
+
+    # A has shape (n, m)
+    # B has shape (n, r)
+    cdef int m = A.shape[1]
+    cdef int n = d.shape[0]
+    cdef int r = B.shape[1]
+
+    out = np.zeros((m, r), dtype=A.dtype)
+    cdef double[:, :] out_view = out
+    cdef double* outp = &out_view[0,0]
+
+    _sparse_dense_sandwich(&Adata[0], &Aindices[0], &Aindptr[0], &B[0,0], &d[0], outp, m, n, r)
+
+    # cdef int i, j, k
+    # cdef int A_idx
+    # cdef double Q
+
+    # for i in range(m):
+    #     for j in range(r):
+    #         for A_idx in range(Aindptr[i], Aindptr[i+1]):
+    #             k = Aindices[A_idx]
+    #             Q = Adata[A_idx] * d[k]
+
+    #             out_view[i, j] = out_view[i, j] + Q * B[k, j]
+
+    # for i in range(m):
+    #     for A_idx in range(Aindptr[i], Aindptr[i+1]):
+    #         k = Aindices[A_idx]
+    #         Q = Adata[A_idx] * d[k]
+    #         for j in range(r):
+    #             out_view[i, j] = out_view[i, j] + Q * B[k, j]
+    
+    # for k in range(n):
+    #     for A_idx in range(Aindptr[k], Aindptr[k+1]):
+    #         i = Aindices[A_idx]
+    #         Q = d[k] * Adata[A_idx]
+    #         for j in range(r):
+    #             # TODO: i, j is lesss efficient
+    #             out_view[i, j] = out_view[i, j] + B[k, j] * Q
+
+    # for k in range(n):
+    #     for j in range(r):
+    #         for A_idx in range(Aindptr[k], Aindptr[k+1]):
+    #             i = Aindices[A_idx]
+    #             Q = d[k] * Adata[A_idx]
+    #             # TODO: i, j is lesss efficient
+    #             out_view[i, j] = out_view[i, j] + B[k, j] * Q
+    return out
+
+
 
 def dense_sandwich(double[:,:] X, double[:] d):
     cdef int m = X.shape[1]
