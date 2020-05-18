e4251dae7af7943f8574ed34712d5eaf69602b07
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2020-05-18 16:48:42 -0400

    Cross-validation (#109)
    * barely did anything
    
    * Moved Link and ExponentialDispersionModel classes to their own files
    
    * Added test
    
    * Added offsets to sklearn code and a test, which is passing
    
    * Tweaking tests
    
    * took out center_predictors option; fixed sign error in gaussian LL
    
    * offset tests
    
    * Fixed a line search bug: refactored 'problems'; getter starting param guess; tightened tolearnces in tests
    
    * Generalized guess_intercept function, moved it, and added a test
    
    * Allow offset to be scalar
    
    * Addressed minor comments from PR
    
    * Sketched stucture for CV
    
    * Pulled out function, fixed some bugs
    
    * Added test for comparison to sklearn ElasticNetCV
    
    * Refactoring
    
    * Refactored to avoid having CV estimator call original estimator
    
    * oops had some kind of git accident last commit
    
    * getting reasonable results now
    
    * Added CV benchmarks and simplified benchmark file structure
    
    * Removed unnecessary stuff
    
    * Minor bug fix in calls to get_link
    
    * Fixed up benchmarks with CV; moved report_diagnostics; more careful tests
    
    * Refactoring main
    
    * Unified command line argument handling in utils and created BenchmarkParams class
    
    * Small change so golden master tests pass
    
    * Fixed bug in running benchmarks
    
    * Towards merge with master
    
    * Fixed issues from PR
    
    * Fixed admm benchmark
    
    Co-authored-by: Elizabeth Santorella <elizabeth.santorella@gmail.com>

--- src/glm_benchmarks/sklearn_fork/__init__.py
+++ src/glm_benchmarks/sklearn_fork/__init__.py
@@ -1,4 +1,9 @@
 from ._distribution import TweedieDistribution
 from ._glm import GeneralizedLinearRegressor
+from ._glm_cv import GeneralizedLinearRegressorCV
 
-__all__ = ["GeneralizedLinearRegressor", "TweedieDistribution"]
+__all__ = [
+    "GeneralizedLinearRegressor",
+    "TweedieDistribution",
+    "GeneralizedLinearRegressorCV",
+]
