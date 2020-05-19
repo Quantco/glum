df71df3eba1e14dae96cab6d00a89e43d793087b
Jan Tilly <jantilly@gmail.com>
2020-05-19 13:58:35 +0200

    Workaround for #126.

--- scripts/run_intermediate_benchmarks.sh
+++ scripts/run_intermediate_benchmarks.sh
@@ -5,6 +5,11 @@ PROBLEM_NAMES="intermediate-insurance-offset-net-poisson, intermediate-insurance
 LIBRARY_NAMES="sklearn-fork, glmnet-python"
 THREADS=8
 
+export GLM_BENCHMARKS_CACHE_SIZE_LIMIT=10737418240  # 10 GB
+export GLM_BENCHMARKS_CACHE=.cache
+
+echo "" > output.log
+
 for NUM_ROWS in 100000 1000000 10000000
 do
     for REG_STRENGTH in 1.0 0.1 0.01 0.0
@@ -14,7 +19,7 @@ do
             echo "---------------------------------"
             echo "NUM_ROWS = ${NUM_ROWS}"
             echo "REG_STRENGTH = ${REG_STRENGTH}"
-            echo "STORAGE = ${STORAGE}"        
+            echo "STORAGE = ${STORAGE}"
             glm_benchmarks_run \
                 --problem_name "${PROBLEM_NAMES}" \
                 --library_name "${LIBRARY_NAMES}" \
@@ -23,12 +28,16 @@ do
                 --storage ${STORAGE} \
                 --regularization_strength ${REG_STRENGTH} \
                 --output_dir ${OUTPUT_DIR}
+
+            glm_benchmarks_analyze \
+                --problem_name "${PROBLEM_NAMES}" \
+                --library_name "${LIBRARY_NAMES}" \
+                --num_rows ${NUM_ROWS} \
+                --threads ${THREADS} \
+                --storage ${STORAGE} \
+                --regularization_strength ${REG_STRENGTH} \
+                --output_dir ${OUTPUT_DIR} >> output.log
+
         done
     done
 done
-
-# TODO: this is currently only showing results for default parameters
-glm_benchmarks_analyze \
-    --problem_name "${PROBLEM_NAMES}" \
-    --library_name "${LIBRARY_NAMES}" \
-    --output_dir ${OUTPUT_DIR}
