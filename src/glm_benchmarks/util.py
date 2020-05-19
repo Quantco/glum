850409640d1bdd18a3138c5bfa34d81de7558f9a
Jan Tilly <jantilly@gmail.com>
2020-05-19 12:38:17 +0200

    Convert to float.

--- src/glm_benchmarks/util.py
+++ src/glm_benchmarks/util.py
@@ -302,7 +302,9 @@ def clear_cache(force=False):
     if cache_location is None:
         return
 
-    cache_size_limit = os.environ.get("GLM_BENCHMARKS_CACHE_SIZE_LIMIT", 1024 ** 3)
+    cache_size_limit = float(
+        os.environ.get("GLM_BENCHMARKS_CACHE_SIZE_LIMIT", 1024 ** 3)
+    )
 
     if force or _get_size_of_cache_directory() > cache_size_limit:
         shutil.rmtree(cache_location)
