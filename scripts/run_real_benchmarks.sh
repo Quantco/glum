68fc5867c4fddef6b1e51ca714892cbb0546d8ef
Jan Tilly <jantilly@gmail.com>
2020-05-24 13:52:35 +0200

    Swap file names.

--- scripts/run_real_benchmarks.sh
+++ scripts/run_real_benchmarks.sh
@@ -3,7 +3,7 @@
 set -uo pipefail
 
 OUTPUT_DIR=$(git rev-parse HEAD)
-PROBLEM_NAMES="intermediate-insurance-weights-net-poisson,intermediate-insurance-weights-l2-poisson"
+PROBLEM_NAMES="real-insurance-weights-net-poisson,real-insurance-weights-l2-poisson"
 LIBRARY_NAMES="sklearn-fork,orig-sklearn-fork,glmnet-python,h2o"
 THREADS=8
 
@@ -37,4 +37,4 @@ glm_benchmarks_analyze \
     --problem_name "${PROBLEM_NAMES}" \
     --library_name "${LIBRARY_NAMES}" \
     --output_dir ${OUTPUT_DIR} \
-    --export "intermediate_results.csv"
+    --export "real_results.csv"
