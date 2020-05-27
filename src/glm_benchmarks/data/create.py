2a65750c8b0980242514099b708900546c2265c5
Marc-Antoine Schmidt <55998405+MarcAntoineSchmidtQC@users.noreply.github.com>
2020-05-27 11:12:16 -0400

    Add binomial distribution to the set of problems and tests (#137)
    * adding binomial
    
    * added binomial objective value
    
    * fixing leftover typos
    
    * fixed binomial for bad problems
    
    * added binomial golden master test
    
    * remove print statement
    
    * clip instead of max and min
    
    * Liz binomial (#138)
    
    * Removed unused starting_mu function; more numerically accurate inverse variance for binomial; offset for objective functions
    
    * reverted inverse link change
    
    Co-authored-by: Elizabeth Santorella <elizabeth.santorella@gmail.com>
    
    * bringing back clipping
    
    * temp commit, still weird issues
    
    * adding some binomial benchmarks to the golden master tests
    
    * cleanup
    
    Co-authored-by: Marc-Antoine Schmidt <mschmidt@wayfair.com>
    Co-authored-by: ElizabethSantorellaQC <55885047+ElizabethSantorellaQC@users.noreply.github.com>
    Co-authored-by: Elizabeth Santorella <elizabeth.santorella@gmail.com>

--- src/glm_benchmarks/data/create.py
+++ src/glm_benchmarks/data/create.py
@@ -326,9 +326,14 @@ def compute_y_exposure(df, distribution):
     elif distribution == "tweedie":
         exposure = df["Exposure"].values
         y = df["ClaimAmountCut"].values / exposure
+    elif distribution == "binomial":
+        exposure = df["Exposure"].values
+        df["HasClaim"] = (df["ClaimNb"] > 0).astype(np.int32)
+        y = df["HasClaim"].values
     else:
         raise ValueError(
-            f"distribution must be one of ['poisson', 'gamma', 'tweedie', 'gaussian'] not {distribution}."
+            "distribution must be one of ['poisson', 'gamma', 'tweedie', 'gaussian', 'binomial'] "
+            f"not {distribution}."
         )
 
     return y, exposure
