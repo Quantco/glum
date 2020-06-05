20c4a2937f1dbe1bee96932085b268c5479065ef
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2020-06-05 18:45:19 -0400

    Column scaling; more categorical tests; tests that sklearn runs on categorical matrices

--- tests/matrix/test_categorical_matrix.py
+++ tests/matrix/test_categorical_matrix.py
@@ -5,14 +5,33 @@ from sklearn.preprocessing import OneHotEncoder
 from quantcore.glm.matrix.categorical_matrix import CategoricalCSRMatrix
 
 
-@pytest.mark.parametrize("vec_dtype", [np.float64, np.float32, np.int64, np.int32])
-def test_csr_dot_categorical(vec_dtype):
+@pytest.fixture
+def cat_vec():
     m = 10
     seed = 0
     np.random.seed(seed)
-    cat = np.random.choice(np.arange(4, dtype=int), m)
-    mat = OneHotEncoder().fit_transform(cat[:, None])
-    cat_mat = CategoricalCSRMatrix(cat)
+    return np.random.choice(np.arange(4, dtype=int), m)
+
+
+@pytest.mark.parametrize("vec_dtype", [np.float64, np.float32, np.int64, np.int32])
+def test_csr_dot_categorical(cat_vec, vec_dtype):
+    mat = OneHotEncoder().fit_transform(cat_vec[:, None])
+    cat_mat = CategoricalCSRMatrix(cat_vec)
     vec = np.random.choice(np.arange(4, dtype=vec_dtype), mat.shape[1])
     res = cat_mat.dot(vec)
     np.testing.assert_allclose(res, mat.dot(vec))
+
+
+def test_tocsr(cat_vec):
+    cat_mat = CategoricalCSRMatrix(cat_vec)
+    res = cat_mat.tocsr().A
+    expected = OneHotEncoder().fit_transform(cat_vec[:, None]).A
+    np.testing.assert_allclose(res, expected)
+
+
+def test_transpose_dot(cat_vec):
+    cat_mat = CategoricalCSRMatrix(cat_vec)
+    other = np.random.random(cat_mat.shape[0])
+    res = cat_mat.transpose_dot(other)
+    expected = OneHotEncoder().fit_transform(cat_vec[:, None]).T.dot(other)
+    np.testing.assert_allclose(res, expected)
