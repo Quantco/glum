Fixed bug with column scaling and to_csc

tests/matrix/test_categorical_matrix.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+from scipy import sparse as sps
 from sklearn.preprocessing import OneHotEncoder
 
 from quantcore.glm.matrix.categorical_matrix import CategoricalMatrix
@@ -14,24 +15,40 @@ def cat_vec():
 
 
 @pytest.mark.parametrize("vec_dtype", [np.float64, np.float32, np.int64, np.int32])
-def test_csr_dot_categorical(cat_vec, vec_dtype):
+@pytest.mark.parametrize("col_mult", [None, [0, -0.1, 2]])
+def test_csr_dot_categorical(cat_vec, vec_dtype, col_mult):
     mat = OneHotEncoder().fit_transform(cat_vec[:, None])
-    cat_mat = CategoricalMatrix(cat_vec)
+    cat_mat = CategoricalMatrix(cat_vec, col_mult)
     vec = np.random.choice(np.arange(4, dtype=vec_dtype), mat.shape[1])
     res = cat_mat.dot(vec)
-    np.testing.assert_allclose(res, mat.dot(vec))
+    np.testing.assert_allclose(res, cat_mat.A.dot(vec))
 
 
-def test_tocsr(cat_vec):
-    cat_mat = CategoricalMatrix(cat_vec)
+@pytest.mark.parametrize("col_mult", [None, [0, -0.1, 2]])
+def test_tocsr(cat_vec, col_mult):
+    cat_mat = CategoricalMatrix(cat_vec, col_mult)
     res = cat_mat.tocsr().A
     expected = OneHotEncoder().fit_transform(cat_vec[:, None]).A
+    if col_mult is not None:
+        expected *= np.array(col_mult)[None, :]
     np.testing.assert_allclose(res, expected)
 
 
-def test_transpose_dot(cat_vec):
-    cat_mat = CategoricalMatrix(cat_vec)
+@pytest.mark.parametrize("col_mult", [None, [0, -0.1, 2]])
+def test_check_csc(cat_vec, col_mult):
+    cat_mat = CategoricalMatrix(cat_vec, col_mult)
+    data, indices, indptr = cat_mat._check_csc()
+    data = np.ones(cat_mat.shape[0], dtype=int) if data is None else data
+    res = sps.csc_matrix((data, indices, indptr), shape=cat_mat.shape)
+    expected = cat_mat.tocsr().tocsc()
+    np.testing.assert_allclose(res.indices, expected.indices)
+    np.testing.assert_allclose(res.indptr, expected.indptr)
+
+
+@pytest.mark.parametrize("col_mult", [None, [0, -0.1, 2]])
+def test_transpose_dot(cat_vec, col_mult):
+    cat_mat = CategoricalMatrix(cat_vec, col_mult)
     other = np.random.random(cat_mat.shape[0])
     res = cat_mat.transpose_dot(other)
-    expected = OneHotEncoder().fit_transform(cat_vec[:, None]).T.dot(other)
+    expected = cat_mat.A.T.dot(other)
     np.testing.assert_allclose(res, expected)