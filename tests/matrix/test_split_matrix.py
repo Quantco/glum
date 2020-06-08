80b924a559d05b9e5fbff5714491f3d4f598229d
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2020-06-08 18:53:11 -0400

    Added categorical times sparse/dense sandwiches

--- tests/matrix/test_split_matrix.py
+++ tests/matrix/test_split_matrix.py
@@ -2,14 +2,14 @@ import numpy as np
 import pytest
 import scipy.sparse as sps
 
+import quantcore.glm.matrix as mx
 from quantcore.glm.matrix.sandwich.sandwich import csr_dense_sandwich
 from quantcore.glm.matrix.split_matrix import SplitMatrix, split_sparse_and_dense_parts
 
 N = 100
 
 
-@pytest.fixture
-def X():
+def make_X() -> np.ndarray:
     X = np.zeros((N, 4))
     X[:, 0] = 1.0
     X[:10, 1] = 0.5
@@ -18,7 +18,12 @@ def X():
     return X
 
 
-def test_split_matrix_init(X: np.ndarray):
+@pytest.fixture
+def X() -> np.ndarray:
+    return make_X()
+
+
+def test_csc_to_split(X: np.ndarray):
     for T, D, S in [(0.05, 4, 0), (0.1, 3, 1), (0.2, 2, 2), (0.3, 2, 2), (1.0, 0, 4)]:
         dense, sparse, dense_ix, sparse_ix = split_sparse_and_dense_parts(
             sps.csc_matrix(X), T
@@ -28,7 +33,23 @@ def test_split_matrix_init(X: np.ndarray):
         assert fully_dense.indices[1].shape[0] == S
 
 
-def test_sandwich_dense(X: np.ndarray):
+@pytest.fixture()
+def split_mat() -> SplitMatrix:
+    X = make_X()
+    threshold = 0.1
+    cat_mat = mx.CategoricalMatrix(np.random.choice(range(4), X.shape[0]))
+    dense, sparse, dense_ix, sparse_ix = split_sparse_and_dense_parts(
+        sps.csc_matrix(X), threshold
+    )
+    cat_start = 1 + max(dense_ix.max(), sparse_ix.max())
+    mat = SplitMatrix(
+        [dense, sparse, cat_mat],
+        [dense_ix, sparse_ix, range(cat_start, cat_start + cat_mat.shape[1])],
+    )
+    return mat
+
+
+def test_sandwich_sparse_dense(X: np.ndarray):
     np.random.seed(0)
     n, k = X.shape
     d = np.random.random((n,))
@@ -38,18 +59,18 @@ def test_sandwich_dense(X: np.ndarray):
     np.testing.assert_allclose(result, expected)
 
 
-def test_sandwich(X: np.ndarray):
+def test_sandwich(split_mat: SplitMatrix):
     for i in range(10):
-        n = np.random.randint(8, 300)
-        m = np.random.randint(2, n)
-        X = sps.random(n, m, density=0.2)
-        v = np.random.rand(n)
-        dense, sparse, dense_ix, sparse_ix = split_sparse_and_dense_parts(
-            sps.csc_matrix(X), 0.2
-        )
-        Xsplit = SplitMatrix([dense, sparse], [dense_ix, sparse_ix])
-        y1 = Xsplit.sandwich(v)
-        y2 = ((X.T.multiply(v)) @ X).toarray()
-        np.testing.assert_allclose(y1, y2, atol=1e-12)
+        v = np.random.rand(split_mat.shape[0])
+        y1 = split_mat.sandwich(v)
+        y2 = (split_mat.A.T * v[None, :]) @ split_mat.A
+        try:
+            np.testing.assert_allclose(y1, y2, atol=1e-12)
+        except AssertionError:
+            tmp = np.abs(y1 - y2)
+            print(np.where(tmp == tmp.max()))
+            import ipdb
+
+            ipdb.set_trace()
         maxdiff = np.max(np.abs(y1 - y2))
         assert maxdiff < 1e-12
