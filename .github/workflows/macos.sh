aac374722e5323ff718aed7d7c281bf70371aff9
Uwe L. Korn <xhochy@users.noreply.github.com>
2020-06-09 18:54:30 +0200

    Add a conda recipe (#188)
    * Add a conda recipe
    
    * Update conda.recipe/meta.yaml
    
    Co-authored-by: Jan Tilly <jantilly@gmail.com>
    
    * Fix package builds
    
    * Install conda-channel-client on MacOS
    
    Co-authored-by: Jan Tilly <jantilly@gmail.com>

--- .github/workflows/macos.sh
+++ .github/workflows/macos.sh
@@ -2,15 +2,10 @@
 
 set -exo pipefail
 
-wget https://github.com/conda-forge/miniforge/releases/download/4.8.3-4/Miniforge3-4.8.3-4-MacOSX-x86_64.sh -O miniforge.sh
-/bin/bash miniforge.sh -b -p /opt/conda
-source /opt/conda/etc/profile.d/conda.sh
-conda activate base
-conda config --system --set auto_update_conda false
-conda config --system --prepend channels ***REMOVED***
-conda config --system --set custom_channels.***REMOVED*** https://${***REMOVED***}:${***REMOVED***}@conda.***REMOVED***
-conda install -y mamba=0.2.12
-mamba install -y conda-build conda-channel-client yq jq
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source ${SCRIPT_DIR}/macos-base.sh $*
+
+mamba install -y yq jq
 
 yq -Y ". + {dependencies: [.dependencies[], \"python=${PYTHON_VERSION}\"] }" environment.yml > /tmp/environment.yml
 mamba env create -f /tmp/environment.yml
