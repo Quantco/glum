7519e13d5d4d183faa16ea539cce07cc1e32f020
Ben Thompson <t.ben.thompson@gmail.com>
2020-06-25 17:44:19 -0400

    Scale columns by default. (#230)
    * Optimizing for Poisson
    
    * ColScaledSpMat for dense.
    
    * Adding max_min_subgrad calculation.
    
    * Debugging active set criterion.
    
    * Active set IRLS working.
    
    * Delete dead code.
    
    * Comment.
    
    * Remove commented out section.
    
    * Implementing limited sandwich for dense.
    
    * Code, why you broken?
    
    * Limited sandwich and CD working.
    
    * Optimization todo comment.
    
    * Limiting sandwich rows.
    
    * Refactor to default back to non-custom objective/grad/hess.
    
    * Revert Dense col scaled. Fix tests.
    
    * Basing hessian update on last update rather than last iteration.
    
    * limited_rmatvec
    
    * limited_matvec
    
    * Refactoring a bit.
    
    * Simplify to have only one place where eta, mu, deviance, gradient, hessian are computed.
    
    * Fix _safe_lin_pred
    
    * Removed profiles.
    
    * Adding diagnostics on updates and active rows.
    
    * Working for L2 and Lasso.
    
    * All benchmark GM tests passing with approximate hessian update.
    
    * Starting to add limited_... functions to non-dense matrices.
    
    * Limited sandwich, dot, transpose_dot for all matrix classes. Not fast yet.
    
    * Working through failing tests.
    
    * Remove ipdb
    
    * All tests green!
    
    * Refactor matrix extensions. Fast sparse column and row restriction.
    
    * Modified csr_dense_sandwich to work with row/col restriction.
    
    * Centralize aligned allocation/deallocation handling
    
    * Clean up dense and sparse dot/transpose_dot implementations.
    
    * Cleaner
    
    * Nasty split matrix bug fix.
    
    * Fix circular import issue.
    
    * Adding hessian_approx parameter.
    
    * Added hessian_approx as a parameter.
    
    * Documenting the approximate hessian update. And fixing a bug for when the new active set is not a subset of the old active set.
    
    * Fix sandwich import.
    
    * move preprocessor to beginning of line.
    
    * Added a missing c file to gitignore
    
    * Addressing PR comments.
    
    * renamed ColScaledMat to StandardizedMat
    
    * Added multipliers to StandardizedMat.
    
    * unstandardizing X means nothing now.
    
    * scaling by default.
    
    * Add test to ensure fit has no side effects.
    
    * Scaling bounds.
    
    * Avoid side effects on bounds arrays.
    
    * remove ipdb
    
    * Update golden master results.
    
    * Always scale to std=1
    
    * Dead code!
    
    * Fix CV GLM.
    
    * Docstring and typing for _standardize.
    
    Co-authored-by: Benjamin Spector <benjamin.spector@quantco.com>

--- src/quantcore/glm/matrix/__init__.py
+++ src/quantcore/glm/matrix/__init__.py
@@ -1,6 +1,6 @@
 from .categorical_matrix import CategoricalMatrix
 from .dense_glm_matrix import DenseGLMDataMatrix
-from .matrix_base import MatrixBase
+from .matrix_base import MatrixBase, one_over_var_inf_to_val
 from .mkl_sparse_matrix import MKLSparseMatrix
 from .split_matrix import SplitMatrix, csc_to_split
 from .standardized_mat import StandardizedMat
@@ -13,4 +13,5 @@ __all__ = [
     "SplitMatrix",
     "CategoricalMatrix",
     "csc_to_split",
+    "one_over_var_inf_to_val",
 ]
