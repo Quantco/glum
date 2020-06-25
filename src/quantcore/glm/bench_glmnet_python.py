32d67c85d9b3756fdb0c4355a0babf82402b226d
MichaelYinQC <michael.yin@quantco.com>
2020-06-25 17:15:22 -0400

    Divided liblinear regularization by number of observations to match other solvers

--- src/quantcore/glm/bench_glmnet_python.py
+++ src/quantcore/glm/bench_glmnet_python.py
@@ -48,6 +48,7 @@ def glmnet_python_bench(
         y=dat["y"].astype(np.float64).copy(),
         family=distribution,
         alpha=l1_ratio,
+        # intr=False,
         standardize=False,
         thresh=benchmark_convergence_tolerance,
     )
