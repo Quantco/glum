9ec600e963a060b5facaee73d1253f06d4bfc40b
Marc-Antoine Schmidt <marc-antoine.schmidt@quantco.com>
2020-07-08 12:41:54 -0400

    more robust data creation

--- src/quantcore/glm/data/__init__.py
+++ src/quantcore/glm/data/__init__.py
@@ -5,6 +5,7 @@ from .create import (
     generate_real_insurance_dataset,
     generate_wide_insurance_dataset,
 )
+from .simulated_glm import simulate_glm_data
 
 __all__ = [
     "generate_intermediate_insurance_dataset",
@@ -12,4 +13,5 @@ __all__ = [
     "generate_wide_insurance_dataset",
     "generate_real_insurance_dataset",
     "create_raw_data",
+    "simulate_glm_data",
 ]
