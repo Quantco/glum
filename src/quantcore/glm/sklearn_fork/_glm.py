3201ea5a1ebc9fa884d6b8d37986d1938c372f8e
MichaelYinQC <michael.yin@quantco.com>
2020-07-08 18:25:14 -0400

    Added tweedie option to get_link

--- src/quantcore/glm/sklearn_fork/_glm.py
+++ src/quantcore/glm/sklearn_fork/_glm.py
@@ -68,7 +68,7 @@ from ._distribution import (
     TweedieDistribution,
     guess_intercept,
 )
-from ._link import IdentityLink, Link, LogitLink, LogLink
+from ._link import IdentityLink, Link, LogitLink, LogLink, TweedieLink
 from ._solvers import (
     IRLSData,
     _cd_solver,
@@ -381,9 +381,13 @@ def get_link(link: Union[str, Link], family: ExponentialDispersionModel) -> Link
         return LogLink()
     if link == "logit":
         return LogitLink()
+    if link == "tweedie":
+        if isinstance(family, TweedieDistribution):
+            return TweedieLink(family.power)
+        raise ValueError("Tweedie link requires Tweedie distribution.")
     raise ValueError(
         """The link must be an instance of class Link or an element of
-        ['auto', 'identity', 'log', 'logit']; got (link={})""".format(
+        ['auto', 'identity', 'log', 'logit', 'tweedie']; got (link={})""".format(
             link
         )
     )
