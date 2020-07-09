266201b607906522b32ffb98f4f98a043bed8b14
Ben Thompson <t.ben.thompson@gmail.com>
2020-07-09 09:37:23 -0400

    Update directory structure to have quantcore/glm and quantcore/glm_benchmarks. Remove quantcore/matrix. (#256)
    * Move benchmarking into quantcore.glm_benchmarks and library to quantcore.glm
    
    * Deprecating the old sklearn_fork name.
    
    * Fix imports and tests pass.
    
    * Add quantcore.matrix dependency.
    
    * Add quantcore.matrix to conda recipe.

--- src/quantcore/glm/sklearn_fork/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._distribution import TweedieDistribution
-from ._glm import GeneralizedLinearRegressor
-from ._glm_cv import GeneralizedLinearRegressorCV
-
-__all__ = [
-    "GeneralizedLinearRegressor",
-    "TweedieDistribution",
-    "GeneralizedLinearRegressorCV",
-]
