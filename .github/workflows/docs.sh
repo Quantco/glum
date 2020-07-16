c0929a23b92427ce4ff00ef736e97dab01bef3c0
Ben Thompson <t.ben.thompson@gmail.com>
2020-07-16 15:49:38 -0400

    Adding Sphinx documentation construction and upload to CI.

--- .github/workflows/docs.sh
+++ .github/workflows/docs.sh
@@ -5,8 +5,11 @@ set -exo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${SCRIPT_DIR}/base.sh $*
 
-yq -Y ". + {dependencies: [.dependencies[], \"python=${PYTHON_VERSION}\"] }" environment.yml > /tmp/environment.yml
+yq -Y '. + {dependencies: [.dependencies[], "python=3.8"] }' environment.yml > /tmp/environment.yml
 mamba env create -f /tmp/environment.yml
 conda activate $(yq -r .name environment.yml)
 pip install --no-use-pep517 --no-deps --disable-pip-version-check -e .
-pytest -nauto tests --doctest-modules src/
+
+pushd $(pwd)/docs
+make html
+popd
