7075ba221c8f2c6e6b6ec14cad361a8aaeb8ffcc
MichaelYinQC <michael.yin@quantco.com>
2020-08-03 12:32:48 -0400

    Added alpha_level parameter for alpha_search scenarios

--- src/quantcore/glm/_glm.py
+++ src/quantcore/glm/_glm.py
@@ -968,7 +968,9 @@ class GeneralizedLinearRegressorBase(BaseEstimator, RegressorMixin):
         else:
             print("solver does not report diagnostics")
 
-    def linear_predictor(self, X: ArrayLike, offset: Optional[ArrayLike] = None):
+    def linear_predictor(
+        self, X: ArrayLike, offset: Optional[ArrayLike] = None, alpha_level: int = None
+    ):
         """Compute the linear_predictor = X*coef_ + intercept_.
 
         Parameters
@@ -990,7 +992,8 @@ class GeneralizedLinearRegressorBase(BaseEstimator, RegressorMixin):
             ensure_2d=True,
             allow_nd=False,
         )
-        xb = X @ self.coef_ + self.intercept_
+        coef = self.coef_ if alpha_level is None else self.coef_path_[alpha_level]
+        xb = X @ coef + self.intercept_
         if offset is None:
             return xb
         return xb + offset
@@ -1000,6 +1003,7 @@ class GeneralizedLinearRegressorBase(BaseEstimator, RegressorMixin):
         X: ShapedArrayLike,
         sample_weight: Optional[ArrayLike] = None,
         offset: Optional[ArrayLike] = None,
+        alpha_level: int = None,
     ):
         """Predict using GLM with feature matrix X.
 
@@ -1030,7 +1034,7 @@ class GeneralizedLinearRegressorBase(BaseEstimator, RegressorMixin):
             ensure_2d=True,
             allow_nd=False,
         )
-        eta = self.linear_predictor(X, offset=offset)
+        eta = self.linear_predictor(X, offset=offset, alpha_level=alpha_level)
         mu = get_link(self.link, get_family(self.family)).inverse(eta)
         weights = _check_weights(sample_weight, X.shape[0], X.dtype)
 
