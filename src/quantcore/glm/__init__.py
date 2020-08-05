43cd7ff8fb69f905fd324cdd9334c506ac00ef4d
Jan Tilly <jantilly@gmail.com>
2020-08-06 00:13:09 +0200

    Expose version (#295)
    * Expose __version__.
    
    * Trigger CI.

--- src/quantcore/glm/__init__.py
+++ src/quantcore/glm/__init__.py
@@ -1,6 +1,13 @@
+import pkg_resources
+
 from ._distribution import TweedieDistribution
 from ._glm import GeneralizedLinearRegressor
 from ._glm_cv import GeneralizedLinearRegressorCV
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except Exception:
+    __version__ = "unknown"
 
 __all__ = [
     "GeneralizedLinearRegressor",
