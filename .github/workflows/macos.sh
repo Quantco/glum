4b58876393e2078dee5614b3969f4b6ec62e655d
Uwe L. Korn <xhochy@users.noreply.github.com>
2020-11-24 21:01:27 +0100

    Switch to setup-miniconda@v2 (#331)

--- .github/workflows/macos.sh
+++ .github/workflows/macos.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 set -exo pipefail
+source ~/.profile
 
 mamba install -y yq jq
 
 yq -Y ". + {dependencies: [.dependencies[], \"python=${PYTHON_VERSION}\"] }" environment.yml > /tmp/environment.yml
 mamba env create -f /tmp/environment.yml
-source activate $(yq -r .name environment.yml)
+conda activate $(yq -r .name environment.yml)
 pip install --no-use-pep517 --no-deps --disable-pip-version-check -e .
 pytest -nauto tests --doctest-modules src/
