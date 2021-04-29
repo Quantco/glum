6b63582066b287e5a42044f0b678238f37fc8ed2
MargueriteBastaQC <marguerite.basta@quantco.com>
2021-04-29 16:56:12 -0700

    update rossman

--- docs/tutorials/rossman/metrics.py
+++ docs/tutorials/rossman/metrics.py
@@ -4,12 +4,15 @@ import numpy as np
 def root_mean_squared_percentage_error(y_true, y_pred):
     """Compute RMSPE."""
     mask = y_true > 0.0
-    return np.sqrt(
-        np.mean(
-            np.power(
-                (np.asanyarray(y_true[mask]) - np.asanyarray(y_pred[mask]))
-                / np.asanyarray(y_true[mask]),
-                2,
+    return (
+        np.sqrt(
+            np.mean(
+                np.power(
+                    (np.asanyarray(y_true[mask]) - np.asanyarray(y_pred[mask]))
+                    / np.asanyarray(y_true[mask]),
+                    2,
+                )
             )
         )
+        * 100
     )
