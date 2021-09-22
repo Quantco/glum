0f153f2e0b396e6ec973f2a397be8acf37710b72
Luca Bittarello <15511539+lbittarello@users.noreply.github.com>
2021-09-22 23:37:20 +0200

    Add evaluation methods to distributions (#405)
    * Small clean-up
    
    * Patch BinomialDistribution.unit_deviance
    
    * First pass
    
    * log_likelihood
    
    * Type police
    
    * Dispersion methods
    
    * Dunn and Smyth (2005)
    
    * Reorganize tests
    
    * Debug
    
    * Tests without weights
    
    * Tests with weights
    
    * Broadcasting
    
    * Log changes
    
    * Comments
    
    * Excessive white space
    
    * Cosmetics
    
    * Speedups

--- src/quantcore/glm/_functions.pyx
+++ src/quantcore/glm/_functions.pyx
@@ -3,9 +3,16 @@
 from cython cimport floating
 from cython.parallel import prange
 
-from libc.math cimport exp, log, fmax
+from libc.math cimport M_PI, ceil, exp, floor, fmax, lgamma, log
 
 import numpy as np
+
+ctypedef fused numeric:
+    short
+    int
+    long
+    float
+    double
 
 # If an argument is readonly, that will fail with a typical floating[:]
 # memoryview. However, const floating[:] causes failures because currently,
@@ -60,6 +67,40 @@ def normal_identity_rowwise_gradient_hessian(
         # the default calculation in _distribution.py
         hessian_rows_out[i] = weights[i]
 
+def normal_log_likelihood(
+    const_floating1d y,
+    const_floating1d weights,
+    const_floating1d mu,
+    floating dispersion,
+):
+    cdef int i  # loop counter
+    cdef floating sum_weights  # helper
+
+    cdef int n = y.shape[0]  # loop length
+    cdef floating ll = 0.0  # output
+
+    for i in prange(n, nogil=True):
+        ll += weights[i] * (y[i] - mu[i]) ** 2
+        sum_weights += weights[i]
+
+    return ll / (2 * dispersion) + sum_weights * log(2 * M_PI * dispersion) / 2
+
+def normal_deviance(
+    const_floating1d y,
+    const_floating1d weights,
+    const_floating1d mu,
+    floating dispersion,
+):
+    cdef int i  # loop counter
+
+    cdef int n = y.shape[0]  # loop length
+    cdef floating D = 0.0  # output
+
+    for i in prange(n, nogil=True):
+        D += weights[i] * (y[i] - mu[i]) ** 2
+
+    return D
+
 def poisson_log_eta_mu_deviance(
     const_floating1d cur_eta,
     const_floating1d X_dot_d,
@@ -76,7 +117,7 @@ def poisson_log_eta_mu_deviance(
     for i in prange(n, nogil=True):
         eta_out[i] = cur_eta[i] + factor * X_dot_d[i]
         mu_out[i] = exp(eta_out[i])
-        # True log likelihood: y * eta - mu
+        # True log likelihood: y * eta - mu - lgamma(1 + y)
         deviance += weights[i] * (y[i] * eta_out[i] - mu_out[i])
     return -2 * deviance
 
@@ -93,6 +134,42 @@ def poisson_log_rowwise_gradient_hessian(
     for i in prange(n, nogil=True):
         gradient_rows_out[i] = weights[i] * (y[i] - mu[i])
         hessian_rows_out[i] = weights[i] * mu[i]
+
+def poisson_log_likelihood(
+    const_floating1d y,
+    const_floating1d weights,
+    const_floating1d mu,
+    floating dispersion,
+):
+    cdef int i  # loop counter
+
+    cdef int n = y.shape[0]  # loop length
+    cdef floating ll = 0.0  # output
+
+    for i in prange(n, nogil=True):
+        ll -= weights[i] * mu[i]
+        if y[i] > 0:
+            ll -= weights[i] * (lgamma(1 + y[i]) - y[i] * log(mu[i]))
+
+    return ll
+
+def poisson_deviance(
+    const_floating1d y,
+    const_floating1d weights,
+    const_floating1d mu,
+    floating dispersion,
+):
+    cdef int i  # loop counter
+
+    cdef int n = y.shape[0]  # loop length
+    cdef floating D = 0.0  # output
+
+    for i in prange(n, nogil=True):
+        D += weights[i] * mu[i]
+        if y[i] > 0:
+            D += weights[i] * y[i] * (log(y[i]) - log(mu[i]) - 1)
+
+    return 2 * D
 
 def gamma_log_eta_mu_deviance(
     const_floating1d cur_eta,
@@ -128,6 +205,43 @@ def gamma_log_rowwise_gradient_hessian(
         gradient_rows_out[i] = weights[i] * (y[i] / mu[i] - 1)
         hessian_rows_out[i] = weights[i] * (y[i] / mu[i])
 
+def gamma_log_likelihood(
+    const_floating1d y,
+    const_floating1d weights,
+    const_floating1d mu,
+    floating dispersion,
+):
+    cdef int i  # loop counter
+    cdef floating ln_y, sum_weights  # helpers
+
+    cdef int n = y.shape[0]  # loop length
+    cdef floating ll = 0.0  # output
+    cdef floating inv_dispersion = 1 / dispersion
+    cdef floating normalization = log(dispersion) * inv_dispersion + lgamma(inv_dispersion)
+
+    for i in prange(n, nogil=True):
+        ln_y = log(y[i])
+        ll += weights[i] * (inv_dispersion * (ln_y - log(mu[i]) - y[i] / mu[i]) - ln_y)
+        sum_weights += weights[i]
+
+    return ll - normalization * sum_weights
+
+def gamma_deviance(
+    const_floating1d y,
+    const_floating1d weights,
+    const_floating1d mu,
+    floating dispersion,
+):
+    cdef int i  # loop counter
+
+    cdef int n = y.shape[0]  # loop length
+    cdef floating D = 0.0  # output
+
+    for i in prange(n, nogil=True):
+        D += weights[i] * (log(mu[i]) - log(y[i]) + y[i] / mu[i] - 1)
+
+    return 2 * D
+
 def tweedie_log_eta_mu_deviance(
     const_floating1d cur_eta,
     const_floating1d X_dot_d,
@@ -147,9 +261,7 @@ def tweedie_log_eta_mu_deviance(
         eta_out[i] = cur_eta[i] + factor * X_dot_d[i]
         mu_out[i] = exp(eta_out[i])
         mu1mp = mu_out[i] ** (1 - p)
-        deviance += weights[i] * mu1mp * (
-            mu_out[i] / (2 - p) - y[i] / (1 - p)
-        )
+        deviance += weights[i] * mu1mp * (mu_out[i] / (2 - p) - y[i] / (1 - p))
     return 2 * deviance
 
 def tweedie_log_rowwise_gradient_hessian(
@@ -171,6 +283,110 @@ def tweedie_log_rowwise_gradient_hessian(
         # This hessian will be positive definite for 1 < p < 2. Don't use it
         # outside that range.
         hessian_rows_out[i] = weights[i] * mu1mp * (mu[i] - (1 - p) * ymm)
+
+def tweedie_deviance(
+    const_floating1d y,
+    const_floating1d weights,
+    const_floating1d mu,
+    floating p,
+):
+    cdef int i  # loop counter
+    cdef floating mu1mp, yo1mp  # helpers
+
+    cdef int n = y.shape[0]  # loop length
+    cdef floating D = 0.0  # output
+
+    for i in prange(n, nogil=True):
+
+        mu1mp = mu[i] ** (1 - p)
+        D += weights[i] * (mu1mp * mu[i]) / (2 - p)
+
+        if y[i] > 0:
+            yo1mp = y[i] / (1 - p)
+            D += weights[i] * ((y[i] ** (1 - p)) * yo1mp / (2 - p) - yo1mp * mu1mp)
+
+    return 2 * D
+
+def tweedie_log_likelihood(
+    const_floating1d y,
+    const_floating1d weights,
+    const_floating1d mu,
+    floating p,
+    floating dispersion,
+):
+    cdef int i  # loop counter
+
+    cdef int n = y.shape[0]  # loop length
+    cdef floating ll = 0.0  # output
+
+    for i in prange(n, nogil=True):
+        ll += weights[i] * _tweedie_unit_loglikelihood(y[i], mu[i], p, dispersion)
+
+    return ll
+
+cdef floating _tweedie_unit_loglikelihood(floating y, floating mu, floating power, floating dispersion) nogil:
+
+    cdef floating kappa, normalization, theta
+
+    if y == 0:
+        return -(mu ** (2 - power)) / (dispersion * (2 - power))
+    else:
+        theta = mu ** (1 - power)
+        kappa = mu * theta / (2 - power)
+        theta = theta / (1 - power)
+        normalization = _tweedie_normalization(y, power, dispersion)
+        return (theta * y - kappa) / dispersion + normalization
+
+cdef floating _tweedie_normalization(floating y, floating power, floating dispersion) nogil:
+
+    cdef int j, j_lower, j_upper
+
+    cdef floating j_max = exp((2 - power) * log(y) - log(dispersion) - log(2 - power))
+    cdef floating w_max = _log_w_j(y, power, dispersion, j_max)
+    cdef floating w_summand = 0.0
+
+    j_lower, j_upper = _sum_limits(y, power, dispersion, j_max)
+
+    for j in range(j_lower, j_upper + 1):
+        w_summand += exp(_log_w_j(y, power, dispersion, j) - w_max)
+
+    return w_max + log(w_summand) - log(y)
+
+cdef (int, int) _sum_limits(floating y, floating power, floating dispersion, floating j_max) nogil:
+
+    cdef floating w_lower
+
+    cdef floating j_lower = 1.0
+    cdef floating j_upper = ceil(j_max)
+    cdef floating w_upper = _log_w_j(y, power, dispersion, j_upper)
+
+    cdef floating w_crt = _log_w_j(y, power, dispersion, j_max) - 37
+    cdef floating w_one = _log_w_j(y, power, dispersion, 1)
+
+    if w_one <= w_crt:
+        j_lower = floor(j_max)
+        w_lower = _log_w_j(y, power, dispersion, j_lower)
+        while w_lower >= w_crt:
+            j_lower -= 1
+            w_lower = _log_w_j(y, power, dispersion, j_lower)
+    while w_upper >= w_crt:
+        j_upper += 1
+        w_upper = _log_w_j(y, power, dispersion, j_upper)
+
+    return int(j_lower), int(j_upper)
+
+cdef floating _log_w_j(floating y, floating power, floating dispersion, numeric j) nogil:
+    cdef floating alpha = (2 - power) / (1 - power)
+    return j * _log_z(y, power, dispersion) - lgamma(1 + j) - lgamma(-alpha * j)
+
+cdef floating _log_z(floating y, floating power, floating dispersion) nogil:
+    cdef floating alpha = (2 - power) / (1 - power)
+    return (
+        alpha * log(power - 1)
+        - alpha * log(y)
+        - (1 - alpha) * log(dispersion)
+        - log(2 - power)
+    )
 
 def binomial_logit_eta_mu_deviance(
     const_floating1d cur_eta,
