fb9fa179247f0f465eac6314be2ecadab2bd0b81
Luca Bittarello <15511539+lbittarello@users.noreply.github.com>
2021-09-22 23:37:20 +0200

    Add evaluation methods to distributions (#405)
    * Small clean-up
    
    * Patch BinomialDistribution.unit_deviance
    
    * First pass
    
    * log_likelihood
    
    * Type police
    
    * Dispersion methods
    
    * Dunn and Smyth (2005)
    
    * Reorganize tests
    
    * Debug
    
    * Tests without weights
    
    * Tests with weights
    
    * Broadcasting
    
    * Log changes
    
    * Comments
    
    * Excessive white space
    
    * Cosmetics
    
    * Speedups

--- tests/glm/test_distribution.py
+++ tests/glm/test_distribution.py
@@ -15,6 +15,7 @@ from quantcore.glm._distribution import (
     PoissonDistribution,
     TweedieDistribution,
 )
+from quantcore.glm._glm import GeneralizedLinearRegressor
 from quantcore.glm._link import IdentityLink, LogitLink, LogLink, TweedieLink
 from quantcore.glm._util import _safe_sandwich_dot
 
@@ -216,3 +217,219 @@ def test_hessian_matrix(family, link, true_hessian):
             [approx, sp.optimize.approx_fprime(xk=coef, f=f, epsilon=1e-5)]
         )
     np.testing.assert_allclose(hessian, approx, rtol=1e-3)
+
+
+@pytest.mark.parametrize("weighted", [False, True])
+def test_poisson_deviance_dispersion_loglihood(weighted):
+
+    # y <- c(0, 0, 1, 2, 3)
+    # glm_model = glm(y ~ 1, family = poisson)
+
+    # glm_model$coefficients  # 0.1823216
+    # sum(glm_model$weights * glm_model$residuals^2)/4  # 1.416679
+    # glm_model$deviance  # 7.176404
+    # logLik(glm_model)  # -7.390977 (df=1)
+
+    regressor = GeneralizedLinearRegressor(
+        alpha=0,
+        family="poisson",
+        fit_intercept=False,
+        gradient_tol=1e-8,
+        check_input=False,
+    )
+
+    y = np.array([0, 0, 1, 2, 3])
+
+    if weighted:
+        y, wgts = np.unique(y, return_counts=True)
+    else:
+        wgts = None
+
+    x = np.ones((len(y), 1))
+    mu = regressor.fit(x, y, sample_weight=wgts).predict(x)
+    family = regressor._family_instance
+
+    # R bases dispersion on the deviance for log_likelihood
+    ll = family.log_likelihood(
+        y, mu, weights=wgts, phi=family.deviance(y, mu, weights=wgts) / 5
+    )
+
+    np.testing.assert_approx_equal(regressor.coef_[0], 0.1823216)
+    np.testing.assert_approx_equal(family.deviance(y, mu, weights=wgts), 7.176404)
+    np.testing.assert_approx_equal(ll, -7.390977)
+
+    # higher tolerance for the dispersion parameter because of numerical precision
+    np.testing.assert_approx_equal(
+        family.dispersion(y, mu, weights=wgts), 1.416679, significant=5
+    )
+
+
+@pytest.mark.parametrize("weighted", [False, True])
+def test_gamma_deviance_dispersion_loglihood(weighted):
+
+    # y <- c(1, 2, 2, 3, 4)
+    # glm_model = glm(y ~ 1, family = Gamma(link = "log"))
+
+    # glm_model$coefficients  # 0.8754687
+    # sum(glm_model$weights * glm_model$residuals^2)/4  # 0.2256944
+    # glm_model$deviance  # 1.012285
+    # logLik(glm_model)  # -7.057068 (df=2)
+
+    regressor = GeneralizedLinearRegressor(
+        alpha=0,
+        family="gamma",
+        fit_intercept=False,
+        gradient_tol=1e-8,
+        check_input=False,
+    )
+
+    y = np.array([1, 2, 2, 3, 4])
+
+    if weighted:
+        y, wgts = np.unique(y, return_counts=True)
+    else:
+        wgts = None
+
+    x = np.ones((len(y), 1))
+    mu = regressor.fit(x, y, sample_weight=wgts).predict(x)
+    family = regressor._family_instance
+
+    # R bases dispersion on the deviance for log_likelihood
+    ll = family.log_likelihood(
+        y, mu, weights=wgts, phi=family.deviance(y, mu, weights=wgts) / 5
+    )
+
+    np.testing.assert_approx_equal(regressor.coef_[0], 0.8754687)
+    np.testing.assert_approx_equal(family.dispersion(y, mu, weights=wgts), 0.2256944)
+    np.testing.assert_approx_equal(family.deviance(y, mu, weights=wgts), 1.012285)
+    np.testing.assert_approx_equal(ll, -7.057068)
+
+
+@pytest.mark.parametrize("weighted", [False, True])
+def test_gaussian_deviance_dispersion_loglihood(weighted):
+
+    # y <- c(-1, -1, 0, 1, 2)
+    # glm_model = glm(y ~ 1, family = gaussian)
+
+    # glm_model$coefficients  # 0.2
+    # sum(glm_model$weights * glm_model$residuals^2)/4  # 1.7
+    # glm_model$deviance  # 6.8
+    # logLik(glm_model)  # 7.863404 (df=2)
+
+    regressor = GeneralizedLinearRegressor(
+        alpha=0,
+        family="normal",
+        fit_intercept=False,
+        gradient_tol=1e-8,
+        check_input=False,
+    )
+
+    y = np.array([-1, -1, 0, 1, 2])
+
+    if weighted:
+        y, wgts = np.unique(y, return_counts=True)
+    else:
+        wgts = None
+
+    x = np.ones((len(y), 1))
+    mu = regressor.fit(x, y, sample_weight=wgts).predict(x)
+    family = regressor._family_instance
+
+    # R bases dispersion on the deviance for log_likelihood
+    ll = family.log_likelihood(
+        y, mu, weights=wgts, phi=family.deviance(y, mu, weights=wgts) / 5
+    )
+
+    np.testing.assert_approx_equal(regressor.coef_[0], 0.2)
+    np.testing.assert_approx_equal(family.dispersion(y, mu, weights=wgts), 1.7)
+    np.testing.assert_approx_equal(family.deviance(y, mu, weights=wgts), 6.8)
+    np.testing.assert_approx_equal(ll, 7.863404)
+
+
+@pytest.mark.parametrize("weighted", [False, True])
+def test_tweedie_deviance_dispersion_loglihood(weighted):
+
+    # library(statmod)  # Tweedie GLMs
+    # library(tweedie)  # Tweedie log likelihood
+
+    # y <- c(0, 0, 1, 2, 3)
+    # glm_model = glm(y ~ 1, family = tweedie(var.power = 1.5, link.power = 0))
+
+    # glm_model$coefficients  # 0.1823216
+    # sum(glm_model$weights * glm_model$residuals^2)/4  # 1.293318
+    # glm_model$deviance  # 10.64769
+    # logLiktweedie(glm_model)  # -8.35485
+
+    regressor = GeneralizedLinearRegressor(
+        alpha=0,
+        family=TweedieDistribution(1.5),
+        fit_intercept=False,
+        gradient_tol=1e-8,
+        check_input=False,
+    )
+
+    y = np.array([0, 0, 1, 2, 3])
+
+    if weighted:
+        y, wgts = np.unique(y, return_counts=True)
+    else:
+        wgts = None
+
+    x = np.ones((len(y), 1))
+    mu = regressor.fit(x, y, sample_weight=wgts).predict(x)
+    family = regressor._family_instance
+
+    # R bases dispersion on the deviance for log_likelihood
+    ll = family.log_likelihood(
+        y, mu, weights=wgts, phi=family.deviance(y, mu, weights=wgts) / 5
+    )
+
+    np.testing.assert_approx_equal(regressor.coef_[0], 0.1823216)
+    np.testing.assert_approx_equal(family.deviance(y, mu, weights=wgts), 10.64769)
+    np.testing.assert_approx_equal(ll, -8.35485)
+
+    # higher tolerance for the dispersion parameter because of numerical precision
+    np.testing.assert_approx_equal(
+        family.dispersion(y, mu, weights=wgts), 1.293318, significant=5
+    )
+
+
+@pytest.mark.parametrize("weighted", [False, True])
+def test_binomial_deviance_dispersion_loglihood(weighted):
+
+    # y <- c(0, 1, 0, 1, 0)
+    # glm_model = glm(y ~ 1, family = binomial)
+
+    # glm_model$coefficients  # -0.4054651
+    # sum(glm_model$weights * glm_model$residuals^2)/4  # 1.25
+    # glm_model$deviance  # 6.730117
+    # logLik(glm_model)  # -3.365058 (df=1)
+
+    regressor = GeneralizedLinearRegressor(
+        alpha=0,
+        family="binomial",
+        fit_intercept=False,
+        gradient_tol=1e-8,
+        check_input=False,
+    )
+
+    y = np.array([0, 1, 0, 1, 0])
+
+    if weighted:
+        y, wgts = np.unique(y, return_counts=True)
+    else:
+        wgts = None
+
+    x = np.ones((len(y), 1))
+    mu = regressor.fit(x, y, sample_weight=wgts).predict(x)
+    family = regressor._family_instance
+
+    # R bases dispersion on the deviance for log_likelihood
+    ll = family.log_likelihood(
+        y, mu, weights=wgts, phi=family.deviance(y, mu, weights=wgts) / 5
+    )
+
+    np.testing.assert_approx_equal(regressor.coef_[0], -0.4054651)
+    np.testing.assert_approx_equal(family.dispersion(y, mu, weights=wgts), 1.25)
+    np.testing.assert_approx_equal(family.deviance(y, mu, weights=wgts), 6.730117)
+    np.testing.assert_approx_equal(ll, -3.365058)
