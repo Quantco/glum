90e10838d29e9cad04a7d3f0df498d59214abd40
Luca Bittarello <15511539+lbittarello@users.noreply.github.com>
2021-09-29 19:00:29 +0200

    Allow for training and predicting with different categories (#420)
    * _align_df_dtypes
    
    * Less strict implementation
    
    * Update docstring
    
    * Update docstring
    
    * Inform instead of warning
    
    * Do not cast numeric columns

--- src/quantcore/glm/_util.py
+++ src/quantcore/glm/_util.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Union
 
 import numpy as np
@@ -5,10 +6,51 @@ import pandas as pd
 from quantcore.matrix import MatrixBase, StandardizedMatrix
 from scipy import sparse
 
+_logger = logging.getLogger(__name__)
+
 
 def _asanyarray(x, **kwargs):
     """``np.asanyarray`` with passthrough for scalars."""
     return x if pd.api.types.is_scalar(x) else np.asanyarray(x, **kwargs)
+
+
+def _align_df_categories(df, dtypes) -> pd.DataFrame:
+    """Align data types for prediction.
+
+    This function checks that categorical columns have same categories in the
+    same order as specified in ``dtypes``. If an entry has a category not
+    specified in ``dtypes``, it will be set to ``numpy.nan``.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+    dtypes : Dict[str, Union[str, type, pandas.core.dtypes.base.ExtensionDtype]]
+    """
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError(f"Expected `pandas.DataFrame'; got {type(df)}.")
+
+    changed_dtypes = {}
+
+    categorical_dtypes = [
+        column
+        for column, dtype in dtypes.items()
+        if pd.api.types.is_categorical_dtype(dtype) and (column in df)
+    ]
+
+    for column in categorical_dtypes:
+        if not pd.api.types.is_categorical_dtype(df[column]):
+            _logger.info(f"Casting {column} to categorical.")
+            changed_dtypes[column] = df[column].astype(dtypes[column])
+        elif list(df[column].cat.categories) != list(dtypes[column].categories):
+            _logger.info(f"Aligning categories of {column}.")
+            changed_dtypes[column] = df[column].cat.set_categories(
+                dtypes[column].categories
+            )
+
+    if changed_dtypes:
+        df = df.assign(**changed_dtypes)
+
+    return df
 
 
 def _safe_lin_pred(
