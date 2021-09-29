953263ba67607c7dfa43b464f58dc3cee7423aed
lbittarello <luca.bittarello@gmail.com>
2021-09-29 08:39:35 +0100

    Do not cast numeric columns

--- tests/glm/test_utils.py
+++ tests/glm/test_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from quantcore.glm._util import _align_df_dtypes
+from quantcore.glm._util import _align_df_categories
 
 
 @pytest.fixture()
@@ -20,7 +20,7 @@ def df():
     )
 
 
-def test_align_df_dtypes_numeric(df):
+def test_align_df_categories_numeric(df):
 
     dtypes = {column: np.float64 for column in df}
 
@@ -29,17 +29,17 @@ def test_align_df_dtypes_numeric(df):
             "x1": np.array([0, 1], dtype="int64"),
             "x2": np.array([0, 1], dtype="bool"),
             "x3": np.array([0, 1], dtype="float64"),
-            "x4": np.array([0, 1], dtype="int64"),
-            "x5": np.array([np.nan, np.nan], dtype="float64"),
-            "x6": np.array([np.nan, np.nan], dtype="float64"),
-            "x7": np.array([np.nan, np.nan], dtype="float64"),
+            "x4": ["0", "1"],
+            "x5": ["a", "b"],
+            "x6": pd.Categorical(["a", "b"]),
+            "x7": pd.Categorical(["a", "b"], categories=["b", "a"]),
         }
     )
 
-    pd.testing.assert_frame_equal(_align_df_dtypes(df, dtypes), expected)
+    pd.testing.assert_frame_equal(_align_df_categories(df, dtypes), expected)
 
 
-def test_align_df_dtypes_categorical(df):
+def test_align_df_categories_categorical(df):
 
     dtypes = {column: pd.CategoricalDtype(["a", "b"]) for column in df}
 
@@ -49,36 +49,36 @@ def test_align_df_dtypes_categorical(df):
             "x2": [np.nan, np.nan],
             "x3": [np.nan, np.nan],
             "x4": [np.nan, np.nan],
-            "x5": ["a", "b"],
-            "x6": ["a", "b"],
-            "x7": ["a", "b"],
+            "x5": pd.Categorical(["a", "b"]),
+            "x6": pd.Categorical(["a", "b"]),
+            "x7": pd.Categorical(["a", "b"]),
         },
         dtype=pd.CategoricalDtype(["a", "b"]),
     )
 
-    pd.testing.assert_frame_equal(_align_df_dtypes(df, dtypes), expected)
+    pd.testing.assert_frame_equal(_align_df_categories(df, dtypes), expected)
 
 
-def test_align_df_dtypes_excess_columns(df):
+def test_align_df_categories_excess_columns(df):
 
-    dtypes = {"x4": np.float64}
+    dtypes = {"x1": np.float64}
 
     expected = pd.DataFrame(
         {
             "x1": np.array([0, 1], dtype="int64"),
             "x2": np.array([0, 1], dtype="bool"),
             "x3": np.array([0, 1], dtype="float64"),
-            "x4": np.array([0, 1], dtype="int64"),
+            "x4": ["0", "1"],
             "x5": ["a", "b"],
             "x6": pd.Categorical(["a", "b"]),
             "x7": pd.Categorical(["a", "b"], categories=["b", "a"]),
         }
     )
 
-    pd.testing.assert_frame_equal(_align_df_dtypes(df, dtypes), expected)
+    pd.testing.assert_frame_equal(_align_df_categories(df, dtypes), expected)
 
 
-def test_align_df_dtypes_missing_columns(df):
+def test_align_df_categories_missing_columns(df):
 
     dtypes = {"x0": np.float64}
 
@@ -94,9 +94,9 @@ def test_align_df_dtypes_missing_columns(df):
         }
     )
 
-    pd.testing.assert_frame_equal(_align_df_dtypes(df, dtypes), expected)
+    pd.testing.assert_frame_equal(_align_df_categories(df, dtypes), expected)
 
 
-def test_align_df_dtypes_not_df():
+def test_align_df_categories_not_df():
     with pytest.raises(TypeError):
-        _align_df_dtypes(np.array([[0], [1]]), {"x0": np.float64})
+        _align_df_categories(np.array([[0], [1]]), {"x0": np.float64})
