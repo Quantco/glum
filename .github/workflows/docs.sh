7bfbec54616d695c2194f26b6054d75ec0011fe3
Ben Thompson <t.ben.thompson@gmail.com>
2021-10-07 12:15:43 -0400

    Add install conda-build and yq where needed.

--- .github/workflows/docs.sh
+++ .github/workflows/docs.sh
@@ -5,6 +5,7 @@ set -exo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${SCRIPT_DIR}/base.sh $*
 
+mamba install -y yq
 yq -Y '. + {dependencies: [.dependencies[], "python=3.8"] }' environment.yml > /tmp/environment.yml
 mamba env create -f /tmp/environment.yml
 conda activate $(yq -r .name environment.yml)
