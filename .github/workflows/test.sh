2a7374b586b7ce671cd57281749c159c4cb10ac7
Ben Thompson <t.ben.thompson@gmail.com>
2021-10-07 15:08:59 -0400

    Remove QC infrastructure dependencies. (#443)
    * Remove QC infrastructure dependencies.
    
    * Add install conda-build and yq where needed.

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -5,6 +5,7 @@ set -exo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${SCRIPT_DIR}/base.sh $*
 
+mamba install -y yq
 yq -Y ". + {dependencies: [.dependencies[], \"python=${PYTHON_VERSION}\"] }" environment.yml > /tmp/environment.yml
 mamba env create -f /tmp/environment.yml
 mamba env update -n $(yq -r .name environment.yml) --file environment-benchmark.yml
