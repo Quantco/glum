566c51c09fb2588e99db20bd24e809836bccf55d
Marc-Antoine Schmidt <marc-antoine.schmidt@quantco.com>
2022-02-21 12:02:10 -0500

    remove python 3.6

--- setup.py
+++ setup.py
@@ -60,7 +60,6 @@ setup(
     license="BSD",
     classifiers=[  # Optional
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
