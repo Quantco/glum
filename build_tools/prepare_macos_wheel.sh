76fe22356ed47d1a9fefaf2f72e1351af354237b
Jan Tilly <jan.tilly@quantco.com>
2023-03-14 20:41:26 +0100

    Revert.

--- build_tools/prepare_macos_wheel.sh
+++ build_tools/prepare_macos_wheel.sh
@@ -4,9 +4,10 @@ set -e
 set -x
 
 if [[ "$CIBW_BUILD" == *-macosx_arm64 ]]; then
-    OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-arm64/llvm-openmp-11.1.0-hf3c4609_1.tar.bz2"
+    CONDA_CHANNEL="conda-forge/osx-arm64"
 else
-    OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-64/llvm-openmp-11.1.0-hda6cdc1_1.tar.bz2"
+    CONDA_CHANNEL="conda-forge/osx-64"
 fi
 
-conda create -y -n build $OPENMP_URL
+conda create -n build -c $CONDA_CHANNEL llvm-openmp
+
