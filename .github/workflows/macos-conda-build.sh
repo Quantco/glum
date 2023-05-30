4a817f7329a434b9189fbbb9ddd576f84c834310
Martin Stancsics <martin.stancsics@quantco.com>
2023-05-30 17:42:47 +0200

    Install conda-build into the base env on macos

--- .github/workflows/macos-conda-build.sh
+++ .github/workflows/macos-conda-build.sh
@@ -2,6 +2,10 @@
 
 set -exo pipefail
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source ${SCRIPT_DIR}/base.sh $*
+conda activate base
+
 mamba install -y conda-build
 # Don't test cross-compiled result (there is no emulation) and use the latest MacOS SDK.
 if grep -q "osx-arm64" .ci_support/${CONDA_BUILD_YML}.yaml; then
