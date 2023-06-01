001dc91513b7f26c7e2e9d0f3d796b1270f4af9b
Jan Tilly <jan.tilly@quantco.com>
2023-06-01 13:16:45 +0200

    Fix path.

--- .github/workflows/issue_628.sh
+++ .github/workflows/issue_628.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+pushd ${SCRIPT_DIR}
+
 python -m venv .env
 source .env/bin/activate
 pip install 'glum==2.1.2' 'numpy==1.23.5' 'tabmat==3.1.2'
