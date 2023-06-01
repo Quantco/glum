45a01cfa3918c66618135ac062c41cd4b7eae3c4
Martin Stancsics <martin.stancsics@quantco.com>
2023-06-01 14:23:48 +0200

    Test cf builds only

--- .github/workflows/issue_628.sh
+++ .github/workflows/issue_628.sh
@@ -8,18 +8,23 @@ set -eo pipefail
 source ~/.profile
 
 # run with latest
-mamba create -n latest 'python=3.9.15' pip
-conda activate latest
-pip install 'glum==2.5.1'
-python issue_628.py
+# mamba create -n latest 'python=3.9.15' pip
+# conda activate latest
+# pip install 'glum==2.5.1'
+# python issue_628.py
 
-# test conda build
-mamba create -n orig-conda 'python=3.9.15' glum=2.1.2 numpy=1.23.5 tabmat=3.1.2
-conda activate orig-conda
+# test conda build - latest
+mamba create -n new-conda 'python=3.9.15' 'glum=2.5.1'
+conda activate new-conda
 python issue_628.py
 
 # reproduce the original issue
-mamba create -n orig 'python=3.9.15' pip
-conda activate orig
-pip install 'glum==2.1.2' 'numpy==1.23.5' 'tabmat==3.1.2'
+# mamba create -n orig 'python=3.9.15' pip
+# conda activate orig
+# pip install 'glum==2.1.2' 'numpy==1.23.5' 'tabmat==3.1.2'
+# python issue_628.py
+
+# test conda build - original
+mamba create -n orig-conda 'python=3.9.15' 'glum=2.1.2' 'numpy=1.23.5' 'tabmat=3.1.2'
+conda activate orig-conda
 python issue_628.py
