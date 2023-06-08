4824fbd03343bfd1cc7f1f93fa8b751c8be0b37b
Martin Stancsics <127219363+MartinStancsicsQC@users.noreply.github.com>
2023-06-08 14:14:27 +0200

    Apply suggestions from code review
    Fix docstrings
    
    Co-authored-by: Luca Bittarello <15511539+lbittarello@users.noreply.github.com>

--- src/glum/_glm.py
+++ src/glum/_glm.py
@@ -1967,8 +1967,8 @@ class GeneralizedLinearRegressor(GeneralizedLinearRegressorBase):
         specify it in parentheses (e.g., ``'tweedie (1.5)'``). The same applies
         for ``'negative.binomial'`` and theta parameter.
 
-    link : {'auto', 'identity', 'log', 'logit', 'cloglog'} or Link, optional
-        (default='auto') The link function of the GLM, i.e. mapping from linear
+    link : {'auto', 'identity', 'log', 'logit', 'cloglog'} or Link, optional (default='auto')
+        The link function of the GLM, i.e. mapping from linear
         predictor (``X * coef``) to expectation (``mu``). Option ``'auto'`` sets
         the link depending on the chosen family as follows:
 
