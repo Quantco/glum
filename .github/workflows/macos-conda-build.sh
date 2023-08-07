bc6a5ec28fc0cc6495573611cde45aacdda905e3
Jan Tilly <jan.tilly@quantco.com>
2023-08-07 18:20:53 +0200

    Use boa in CI. (#673)

--- .github/workflows/macos-conda-build.sh
+++ .github/workflows/macos-conda-build.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${SCRIPT_DIR}/base.sh $*
 conda activate base
 
-mamba install -y conda-build -c conda-forge
+mamba install -y conda-build boa -c conda-forge
 # Don't test cross-compiled result (there is no emulation) and use the latest MacOS SDK.
 if grep -q "osx-arm64" .ci_support/${CONDA_BUILD_YML}.yaml; then
   CONDA_BUILD_ARGS="--no-test"
@@ -16,4 +16,4 @@ CONDA_BUILD_SYSROOT:
  - "${CONDA_BUILD_SYSROOT}"
 EOF
 fi
-conda build -m .ci_support/${CONDA_BUILD_YML}.yaml conda.recipe ${CONDA_BUILD_ARGS:-}
+conda mambabuild -m .ci_support/${CONDA_BUILD_YML}.yaml conda.recipe ${CONDA_BUILD_ARGS:-}
