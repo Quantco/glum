745a6a0158d3d1ef2512cd39f28303a4e4f07df7
Matthias Schmidtblaicher <matthias.schmidtblaicher@quantco.com>
2024-04-26 21:00:29 +0200

    replace deprecated scipy.sparse.*_matrix.A

--- src/glum_benchmarks/data/simulated_glm.py
+++ src/glum_benchmarks/data/simulated_glm.py
@@ -104,7 +104,7 @@ def simulate_glm_data(
 
     # Creating sparse component
     sparse_feature_names = [f"sparse{i}" for i in range(sparse_features)]
-    X_sparse = sps.random(n_rows, sparse_features, density=sparse_density).A
+    X_sparse = sps.random(n_rows, sparse_features, density=sparse_density).toarray()
     X_sparse = pd.DataFrame(data=X_sparse, columns=sparse_feature_names)
     coefs_sparse = rand.choice([0, 1, -1], size=sparse_features)
     coefs_sparse = pd.Series(data=coefs_sparse, index=sparse_feature_names)
