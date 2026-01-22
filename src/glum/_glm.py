Fix data leakage in housing data generation (#967)
* Fix data leakage in housing data generation

* Fix mypy type annotation errors

src/glum/_glm.py
@@ -1188,7 +1188,9 @@ class GeneralizedLinearRegressorBase(skl.base.RegressorMixin, skl.base.BaseEstim
             num_restrictions = len(R_indices)
             if num_restrictions == 0:
                 raise ValueError(f"term {term} is not in the model")
-            R_current = np.zeros((num_restrictions, len(beta)), dtype=np.float64)
+            R_current: np.ndarray = np.zeros(
+                (num_restrictions, len(beta)), dtype=np.float64
+            )
             R_current[np.arange(num_restrictions), R_indices] = 1.0
             R_list.append(R_current)
 

src/glum/_solvers.py
@@ -217,13 +217,13 @@ def build_hessian_delta(
         return delta
 
     if P2.ndim == 1:
-        idiag = np.arange(start=idx, stop=delta.shape[0])
+        idiag = np.arange(idx, int(delta.shape[0]))
         delta[(idiag, idiag)] += P2[active_cols_non_intercept]
     else:
         if sparse.issparse(P2):
             is_diagonal = P2.nnz == P2.shape[0] and (P2.data == P2.diagonal()).all()
             if is_diagonal:
-                idiag = np.arange(start=idx, stop=delta.shape[0])
+                idiag = np.arange(idx, int(delta.shape[0]))
                 delta[(idiag, idiag)] += P2.data[active_cols_non_intercept]
             else:
                 delta[idx:, idx:] += P2.toarray()[